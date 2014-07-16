ca562d95261b182847d5125c0d7c2d5ebea4d690
Joosep Pata <joosep.pata@gmail.com>
2014-07-16 15:53:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 export SCRAM_ARCH=slc6_amd64_gcc481
 #cmsrel CMSSW_7_0_6_patch1
 scram project -n CMSSW CMSSW CMSSW_7_0_6_patch1 
-cd CMSSW_7_0_6_patch1/src/
+cd CMSSW/src/
 cmsenv
 git cms-addpkg PhysicsTools/PatAlgos
 git cms-merge-topic 4330
