2b93398d90fc2dddf40cc34b6b177d7ea16d85a2
Joosep Pata <joosep.pata@cern.ch>
2014-07-16 16:48:29 +0300

    version-agnostic cmssw

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 export SCRAM_ARCH=slc6_amd64_gcc481
-cmsrel CMSSW_7_0_6_patch1
+#cmsrel CMSSW_7_0_6_patch1
+scram project -n CMSSW CMSSW CMSSW_7_0_6_patch1 
 cd CMSSW_7_0_6_patch1/src/
 cmsenv
 git cms-addpkg PhysicsTools/PatAlgos
