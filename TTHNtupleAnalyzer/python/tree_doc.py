0e7edcec394d440805da31a7176e0cfb1bb3c7c5
Joosep Pata <joosep.pata@cern.ch>
2014-08-28 13:59:21 +0300

    cleanup, rename config

--- TTHNtupleAnalyzer/python/tree_doc.py
+++ TTHNtupleAnalyzer/python/tree_doc.py
@@ -3,7 +3,9 @@ tf = ROOT.TFile(sys.argv[1])
 tt = tf.Get("tthNtupleAnalyzer/events")
 brs = sorted([b for b in tt.GetListOfBranches()])
 
-for br in brs:
-    bn = br.GetName()
-    ls = br.GetListOfLeaves()
-    print "| %s | %s | | |" % (bn, ls[0].GetTypeName())
+#for br in brs:
+#    bn = br.GetName()
+#    ls = br.GetListOfLeaves()
+#    print "| %s | %s | | |" % (bn, ls[0].GetTypeName())
+
+print tt.GetEntries()
