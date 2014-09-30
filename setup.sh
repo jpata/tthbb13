3188a328b6f5f713b25062a34dc1aa6c85f5aedf
Joosep Pata <joosep.pata@cern.ch>
2014-09-30 13:19:11 +0300

    downgrade from cmssw 7_2

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export SCRAM_ARCH=slc6_amd64_gcc481
 #cmsrel CMSSW_7_0_6_patch1
-scram project -n CMSSW CMSSW CMSSW_7_2_0_pre5 
+scram project -n CMSSW CMSSW CMSSW_7_0_9_patch1
 cd CMSSW/src/
 cmsenv
 git cms-addpkg PhysicsTools/PatAlgos
