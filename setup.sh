06b18a3f4c5d4afb79c54bcd980d86c922ce6984
Joosep Pata <joosep.pata@cern.ch>
2014-09-30 12:43:43 +0300

    updated cmssw version, libraries

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 export SCRAM_ARCH=slc6_amd64_gcc481
 #cmsrel CMSSW_7_0_6_patch1
-scram project -n CMSSW CMSSW CMSSW_7_0_6_patch1 
+scram project -n CMSSW CMSSW CMSSW_7_2_0_pre5 
 cd CMSSW/src/
 cmsenv
 git cms-addpkg PhysicsTools/PatAlgos
-git cms-merge-topic 4330
-mkdir TTH
+#git cms-merge-topic 4330
+git clone https://github.com/jpata/tthbb13.git TTH
+cp TTH/MEAnalysis/libs/*.so CMSSW/lib/$SCRAM_ARCH/
 scram b -j 20
