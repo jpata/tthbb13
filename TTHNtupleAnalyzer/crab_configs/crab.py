464e7123be850e4f72dcdbc896ee7d08b95ed684
Joosep Pata <joosep.pata@cern.ch>
2014-10-17 18:24:10 +0300

    store output in CH, smaller jobs

--- TTHNtupleAnalyzer/crab_configs/crab.py
+++ TTHNtupleAnalyzer/crab_configs/crab.py
@@ -3,7 +3,8 @@ config = Configuration()
 
 config.section_("General")
 config.General.requestName = 'WD_TTJets'
-config.General.workArea = 'crab_TTH_Oct15'
+config.General.workArea = 'crab_TTH_Oct17'
+config.General.saveLogs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
@@ -15,10 +16,10 @@ config.section_("Data")
 config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v2/MINIAODSIM'
 config.Data.dbsUrl = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 20
-#config.Data.publication = True
+config.Data.unitsPerJob = 1
+config.Data.publication = False
 #config.Data.publishDbsUrl = 'phys03'
 #config.Data.publishDataName = 'tth_ntp_test1'
 
 config.section_("Site")
-config.Site.storageSite = "T2_EE_Estonia"
+config.Site.storageSite = "T2_CH_CSCS"
