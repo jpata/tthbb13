2e234c4bb76d7af6a1a640ae0f19458fd6df606e
Joosep Pata <joosep.pata@cern.ch>
2014-10-20 10:55:11 +0300

    added checkout of new branch

--- setup.sh
+++ setup.sh
@@ -17,10 +17,12 @@ git cms-addpkg RecoJets/JetProducers
 
 #get the TTH code
 git clone https://github.com/jpata/tthbb13.git TTH
+cd TTH
+git checkout toptagger_merge_7_0
+cd ..
 cp TTH/MEAnalysis/libs/*.so ../lib/$SCRAM_ARCH/
 
 #to apply a the top tagger as a patch
-cd ..
 git apply TTH/0001-merged-HepTopTagger.patch
 
 scram setup lhapdf
