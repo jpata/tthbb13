b00213d7dc07f13296eea4340f2483845eecc191
Joosep Pata <joosep.pata@cern.ch>
2014-11-05 16:53:54 +0200

    updated top tagger version

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ git cms-addpkg RecoJets/JetProducers
 #git remote add gkasieczka https://github.com/gkasieczka/cmssw.git
 #git fetch -a gkasieczka
 #git merge 8cb31b5587b7e73780d58976a8d174c01330cf44
+#toptagger merged from branch CMSSW_7_2_X / htt-dev
 
 #get the TTH code
 git clone https://github.com/jpata/tthbb13.git TTH
@@ -28,6 +29,7 @@ cp TTH/MEAnalysis/libs/*.so ../lib/$SCRAM_ARCH/
 
 #to apply a the top tagger as a patch
 #git apply --check TTH/0001-merged-HepTopTagger.patch && git apply TTH/0001-merged-HepTopTagger.patch  
-git apply -v --ignore-whitespace TTH/0001-merged-HepTopTagger.patch
+#git apply -v --ignore-whitespace TTH/0001-merged-HepTopTagger.patch
+git apply -3 --ignore-whitespace --ignore-space-change TTH/0002-merged-HepTopTagger.patch
 
 scram setup lhapdf
