6ffd387d977a36f5429d39939c7ee1fffd4e228c
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-11-19 10:27:45 +0100

    Undo accidental change to ttbar_cfg.py

--- TTHNtupleAnalyzer/test/ttbar_cfg.py
+++ TTHNtupleAnalyzer/test/ttbar_cfg.py
@@ -2,9 +2,7 @@
 #used for scram b runtests (automatic testing)
 from TTH.TTHNtupleAnalyzer.Main_cfg import *
 
-
-
 process.source.fileNames = cms.untracked.vstring([
-		'/store/mc/Spring14miniaod/TTbarH_HToBB_M-125_13TeV_pythia6/MINIAODSIM/PU20bx25_POSTLS170_V5-v1/00000/0E97DD3E-2209-E411-8A04-003048945312.root'
+		'/store/results/top/StoreResults/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/USER/Spring14dr_PU_S14_POSTLS170_V6AN1_miniAOD706p1_814812ec83fce2f620905d2bb30e9100-v2/00000/0012F41F-FA17-E411-A1FF-0025905A48B2.root'
 ])
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
