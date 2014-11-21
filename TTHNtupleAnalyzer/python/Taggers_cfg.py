4650f0d5cd6cea9adac9edee090da99c961edd35
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-11-21 17:49:39 +0100

    Added config file and branches for fj studies

--- TTHNtupleAnalyzer/python/Taggers_cfg.py
+++ TTHNtupleAnalyzer/python/Taggers_cfg.py
@@ -46,6 +46,22 @@ process.chs = cms.EDFilter("CandPtrSelector", src = cms.InputTag("packedPFCandid
 # Add stand-alone fat-jet collection 
 from RecoJets.JetProducers.PFJetParameters_cfi import *
 from RecoJets.JetProducers.AnomalousCellParameters_cfi import *
+
+# First create the ungroomed CA08 and CA15 fatjet collections:
+
+# CA, R=0.8, pT > 200 GeV
+process.ca08PFJetsCHS = cms.EDProducer(
+        "FastjetJetProducer",
+        PFJetParameters,
+        AnomalousCellParameters,
+        jetAlgorithm = cms.string("CambridgeAachen"),
+        rParam       = cms.double(0.8),
+    )
+process.ca08PFJetsCHS.src      = cms.InputTag("chs")
+process.ca08PFJetsCHS.jetPtMin = cms.double(200)
+
+
+# CA, R=1.5, pT > 200 GeV
 process.ca15PFJetsCHS = cms.EDProducer(
         "FastjetJetProducer",
         PFJetParameters,
@@ -57,11 +73,103 @@ process.ca15PFJetsCHS.src      = cms.InputTag("chs")
 process.ca15PFJetsCHS.jetPtMin = cms.double(200)
 
 
-# Calculate n-subjettiness for stand-alone fatjets
-process.Njettiness = cms.EDProducer("NjettinessAdder",
-                                    src=cms.InputTag("ca15PFJetsCHS"),
-                                    cone=cms.double(1.5)
+# Add grooming for the CA08 and CA15 fatjets
+process.ca08PFJetsCHSFiltered = process.ca08PFJetsCHS.clone(
+    useFiltering = cms.bool(True),
+    nFilt = cms.int32(3),
+    rFilt = cms.double(0.3),
+    useExplicitGhosts = cms.bool(True),
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
+    )
+
+process.ca08PFJetsCHSPruned = process.ca08PFJetsCHS.clone(
+    cms.PSet(nFilt = cms.int32(2),
+             zcut = cms.double(0.1),
+             rcut_factor = cms.double(0.5)),
+    usePruning = cms.bool(True),
+    useExplicitGhosts = cms.bool(True),
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
+    )
+
+process.ca08PFJetsCHSTrimmed = process.ca08PFJetsCHS.clone(
+    useTrimming = cms.bool(True),
+    rFilt = cms.double(0.2),
+    trimPtFracMin = cms.double(0.03),
+    useExplicitGhosts = cms.bool(True)
+    )
+
+process.ca15PFJetsCHSFiltered = process.ca15PFJetsCHS.clone(
+    useFiltering = cms.bool(True),
+    nFilt = cms.int32(3),
+    rFilt = cms.double(0.3),
+    useExplicitGhosts = cms.bool(True),
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
+    )
+
+process.ca15PFJetsCHSPruned = process.ca15PFJetsCHS.clone(
+    cms.PSet(nFilt = cms.int32(2),
+             zcut = cms.double(0.1),
+             rcut_factor = cms.double(0.5)),
+    usePruning = cms.bool(True),
+    useExplicitGhosts = cms.bool(True),
+    writeCompound = cms.bool(True),
+    jetCollInstanceName=cms.string("SubJets")
+    )
+
+process.ca15PFJetsCHSTrimmed = process.ca15PFJetsCHS.clone(
+    useTrimming = cms.bool(True),
+    rFilt = cms.double(0.2),
+    trimPtFracMin = cms.double(0.03),
+    useExplicitGhosts = cms.bool(True)
+    )
+
+
+# Nsubjettiness for groomed and ungroomed fatjets
+process.NjettinessCA08 = cms.EDProducer("NjettinessAdder",
+                                        src=cms.InputTag("ca08PFJetsCHS"),
+                                        cone=cms.double(0.8)
                             )
+
+process.NjettinessCA08Filtered = cms.EDProducer("NjettinessAdder",
+                                                src=cms.InputTag("ca08PFJetsCHSFiltered"),
+                                                cone=cms.double(0.8)
+                            )
+
+process.NjettinessCA08Pruned = cms.EDProducer("NjettinessAdder",
+                                                src=cms.InputTag("ca08PFJetsCHSPruned"),
+                                                cone=cms.double(0.8)
+                            )
+
+process.NjettinessCA08Trimmed = cms.EDProducer("NjettinessAdder",
+                                                src=cms.InputTag("ca08PFJetsCHSTrimmed"),
+                                                cone=cms.double(0.8)
+                            )
+
+process.NjettinessCA15 = cms.EDProducer("NjettinessAdder",
+                                        src=cms.InputTag("ca15PFJetsCHS"),
+                                        cone=cms.double(1.5)
+                            )
+
+process.NjettinessCA15Filtered = cms.EDProducer("NjettinessAdder",
+                                                src=cms.InputTag("ca15PFJetsCHSFiltered"),
+                                                cone=cms.double(1.5)
+                            )
+
+process.NjettinessCA15Pruned = cms.EDProducer("NjettinessAdder",
+                                                src=cms.InputTag("ca15PFJetsCHSPruned"),
+                                                cone=cms.double(1.5)
+                            )
+
+process.NjettinessCA15Trimmed = cms.EDProducer("NjettinessAdder",
+                                                src=cms.InputTag("ca15PFJetsCHSTrimmed"),
+                                                cone=cms.double(1.5)
+                            )
+
+
+
 
 process.tthNtupleAnalyzer = cms.EDAnalyzer('TTHNtupleAnalyzer',
 	isMC = cms.bool(True),
@@ -81,12 +189,13 @@ process.tthNtupleAnalyzer = cms.EDAnalyzer('TTHNtupleAnalyzer',
         triggerIdentifiersForMatching = cms.vstring([]),
 
         # take ca15PFJetsCHS jets, add the Njettiness values and store them as jet_fat
-        fatjetsObjects  = cms.vstring(['ca15PFJetsCHS']),
-        fatjetsNsubs    = cms.vstring(['Njettiness']),
-        fatjetsBranches = cms.vstring(['fat']),
+        fatjetsObjects  = cms.vstring(  ['ca08PFJetsCHS',  'ca08PFJetsCHSFiltered',  'ca08PFJetsCHSPruned',  'ca08PFJetsCHSTrimmed',  'ca15PFJetsCHS',  'ca15PFJetsCHSFiltered',  'ca15PFJetsCHSPruned',  'ca15PFJetsCHSTrimmed'  ]),
+        fatjetsNsubs    = cms.vstring(  ['NjettinessCA08', 'NjettinessCA08Filtered', 'NjettinessCA08Pruned', 'NjettinessCA08Trimmed', 'NjettinessCA15', 'NjettinessCA15Filtered', 'NjettinessCA15Pruned', 'NjettinessCA15Trimmed' ]),
+        fatjetsBranches = cms.vstring(  ['ca08',           'ca08filtered',           'ca08pruned',           'ca08trimmed',           'ca15',           'ca15filtered',           'ca15pruned',           'ca15trimmed'           ]),
+        fatjetsIsBasicJets = cms.vint32([0,                 1,                        1,                      0,                       0,                1,                        1,                      0]),                                           
 
-        httObjects  = cms.vstring(['HTTJetsCHS', 'MultiRHTTJetsCHS']),                                           
-        httBranches = cms.vstring(['toptagger', 'toptagger2']),                                           
+        httObjects  = cms.vstring(['LooseMultiRHTTJetsCHS']),                                           
+        httBranches = cms.vstring(['looseMultiRHTT']),                                           
         
 	jetMult_min   = cms.untracked.int32(-99),
 	jetPt_min     = cms.untracked.double(15.),
@@ -111,27 +220,8 @@ process.load('RecoJets.JetProducers.caTopTaggers_cff')
 from RecoJets.JetProducers.AnomalousCellParameters_cfi import *
 from RecoJets.JetProducers.PFJetParameters_cfi import *
 
-process.HTTJetsCHS = cms.EDProducer(
-	"HTTTopJetProducer",
-	PFJetParameters.clone(src = cms.InputTag('chs'),
-						  doAreaFastjet = cms.bool(True),
-						  doRhoFastjet = cms.bool(False),
-						  jetPtMin = cms.double(100.0)
-					   ),
-	AnomalousCellParameters,
-	algorithm = cms.int32(1),
-	jetAlgorithm = cms.string("CambridgeAachen"),
-	rParam = cms.double(1.5),
-	mode = cms.int32(0),
-	minFatjetPt = cms.double(200.),
-	minCandPt = cms.double(200.),
-	minSubjetPt = cms.double(30.),
-	writeCompound = cms.bool(True),
-	minCandMass = cms.double(0.),
-	maxCandMass = cms.double(20000.),
-)
 
-process.MultiRHTTJetsCHS = cms.EDProducer(
+process.LooseMultiRHTTJetsCHS = cms.EDProducer(
      "HTTTopJetProducer",
      PFJetParameters.clone( src = cms.InputTag('chs'),
                             doAreaFastjet = cms.bool(True),
@@ -148,9 +238,12 @@ process.MultiRHTTJetsCHS = cms.EDProducer(
      minCandPt = cms.double(200.),
      minSubjetPt = cms.double(30.),
      writeCompound = cms.bool(True),
-     minCandMass = cms.double(100.),
-     maxCandMass = cms.double(250.),
-     massRatioWidth = cms.double(30.),
+     minCandMass = cms.double(0.),
+     maxCandMass = cms.double(1000),
+     massRatioWidth = cms.double(100.),
+     minM23Cut = cms.double(0.),
+     minM13Cut = cms.double(0.),
+     maxM13Cut = cms.double(2.),
 )
 
 
@@ -159,13 +252,31 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.GlobalTag.globaltag = "PLS170_V7AN1"
 
 process.p = cms.Path(
-	#process.hepTopTagPFJetsCHS *
-	#process.hepTopTagInfos *
         process.chs *
+
+        process.ca08PFJetsCHS *
         process.ca15PFJetsCHS *
-        process.Njettiness *        
-	process.HTTJetsCHS *
-	process.MultiRHTTJetsCHS *
+
+        process.ca08PFJetsCHSFiltered * 
+        process.ca08PFJetsCHSPruned   * 
+        process.ca08PFJetsCHSTrimmed  * 
+
+        process.ca15PFJetsCHSFiltered * 
+        process.ca15PFJetsCHSPruned   * 
+        process.ca15PFJetsCHSTrimmed  * 
+
+        process.NjettinessCA08 *        
+        process.NjettinessCA08Filtered *        
+        process.NjettinessCA08Pruned *        
+        process.NjettinessCA08Trimmed *        
+
+        process.NjettinessCA15 *        
+        process.NjettinessCA15Filtered *        
+        process.NjettinessCA15Pruned *        
+        process.NjettinessCA15Trimmed *        
+
+	process.LooseMultiRHTTJetsCHS * 
+       
 	process.tthNtupleAnalyzer
 )
 
