8f50bb1502c7d1e8fecf8e693d811f1e5ebbfba4
Joosep Pata <joosep.pata@cern.ch>
2014-11-24 18:54:05 +0200

    new tags for hadd and replica scripts

--- TTHNtupleAnalyzer/utils/hadd-step2.sh
+++ TTHNtupleAnalyzer/utils/hadd-step2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-hadd -f tthbb_step2.root ~/tth/v1/MEAnalysis_all_rec_std_TTHBB125_p*.root
-hadd -f ttbar_step2.root ~/tth/v1/MEAnalysis_all_rec_std_TTJets*.root
+SDIR=~/tth/s1_nov19_3a4602f__s2_b7e13a1/
+hadd -f /scratch/joosep/tth/step2/tthbb.root $SDIR/MEAnalysis_all_rec_std_TTHBB125_p*.root
+hadd -f /scratch/joosep/tth/step2/ttjets.root $SDIR/MEAnalysis_all_rec_std_TTJets*.root
--- TTHNtupleAnalyzer/utils/replica-step2.sh
+++ TTHNtupleAnalyzer/utils/replica-step2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for site in T2_EE_Estonia T3_CH_PSI; do
-	python ~/util/data_replica.py --delete --from LOCAL --to $site to-copy-step2.txt /store/user/jpata/tth/step2/
+	python ~/util/data_replica.py --delete --from LOCAL --to $site to-copy-step2.txt /store/user/jpata/tth/step2/s1_nov19_3a4602f__s2_b7e13a1
 done
