134fefe160291d089fbae7150f0c9424e68a12e2
Joosep Pata <joosep.pata@cern.ch>
2014-11-24 18:25:28 +0200

    added util scripts

---
+++ TTHNtupleAnalyzer/utils/hadd-step1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+hadd -f /scratch/joosep/ttjets.root /hdfs/cms/store/user/jpata/tth/step1/nov19_3a4602f/TTJets_MSDecaysCKM/*.root
+hadd -f /scratch/joosep/tthbb.root /hdfs/cms/store/user/jpata/tth/step1/nov19_3a4602f/TTbarH_HToBB/*.root
---
+++ TTHNtupleAnalyzer/utils/hadd-step2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hadd -f tthbb_step2.root ~/tth/v1/MEAnalysis_all_rec_std_TTHBB125_p*.root
+hadd -f ttbar_step2.root ~/tth/v1/MEAnalysis_all_rec_std_TTJets*.root
