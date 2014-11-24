7ad32a96b8298b27868ea0d3afc018f2bb11da31
Joosep Pata <joosep.pata@cern.ch>
2014-11-24 18:25:14 +0200

    added nickname map

--- MEAnalysis/python/samples_v1.py
+++ MEAnalysis/python/samples_v1.py
@@ -18,3 +18,11 @@ samples = cms.VPSet(
         xSec     = cms.double(831.76)
     ),
 )
+
+
+nickname_map = {
+	"ttjets": "/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v2/MINIAODSIM",
+	"tthbb": "/TTbarH_HToBB_M-125_13TeV_pythia6/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM",
+	"ttjets_pu40bx50": "/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-141029_PU40bx50_PLS170_V6AN2-v1/MINIAODSIM",
+	"tthbb_pu40bx50": "/TTbarH_HToBB_M-125_13TeV_pythia6/Spring14miniaod-141029_PU40bx50_PLS170_V6AN2-v1/MINIAODSIM"
+}
