04761c0405ae631883e46b5883489d1641f33fb0
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-12-08 22:32:01 +0100

    Submission of ntop_v3

--- TTHNtupleAnalyzer/crab_configs/ntop.py
+++ TTHNtupleAnalyzer/crab_configs/ntop.py
@@ -3,8 +3,22 @@
 from TTH.TTHNtupleAnalyzer.CrabHelpers import submit
 
 # What to do
-version = "v2"
-li_samples = ["zprime_m2000_1p_13tev", "qcd_300_470_pythia6_13tev"]
+version = "v3"
+li_samples = ["qcd_120_170_pythia6_13tev",
+              "qcd_170_300_pythia6_13tev",
+              "qcd_300_470_pythia6_13tev",
+              "qcd_470_600_pythia6_13tev",
+              "qcd_600_800_pythia6_13tev",
+              "qcd_800_1000_pythia6_13tev",
+              "qcd_1000_1400_pythia6_13tev",
+              "qcd_1400_1800_pythia6_13tev",
+              "qcd_1800_inf_pythia6_13tev",
+              "zprime_m1000_1p_13tev",
+              "zprime_m1250_1p_13tev",
+              "zprime_m1500_1p_13tev",
+              "zprime_m2000_1p_13tev",
+              "zprime_m3000_1p_13tev",
+              "zprime_m4000_1p_13tev"]
 
 # Submit all samples
 for sample_shortname in li_samples:
