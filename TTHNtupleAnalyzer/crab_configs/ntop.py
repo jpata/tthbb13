988d952ab3d3e765160226e6fafae0409aa9982d
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-12-11 17:47:13 +0100

    ntop_v7: Switched to Pythia8 QCD

--- TTHNtupleAnalyzer/crab_configs/ntop.py
+++ TTHNtupleAnalyzer/crab_configs/ntop.py
@@ -15,23 +15,16 @@ from TTH.TTHNtupleAnalyzer.CrabHelpers import submit, status, download, hadd
 
 # Ntuple name/version and samples to include
 name = "ntop"
-version = "v6"
-li_samples = [#"qcd_120_170_pythia6_13tev", 
-              #"qcd_170_300_pythia6_13tev", 
-              #"qcd_300_470_pythia6_13tev", 
-              #"qcd_470_600_pythia6_13tev", 
-              #"qcd_600_800_pythia6_13tev", 
-              "qcd_800_1000_pythia6_13tev",
-              #"qcd_1000_1400_pythia6_13tev", 
-              #"qcd_1400_1800_pythia6_13tev", 
-              #"qcd_1800_inf_pythia6_13tev", 
-              #"zprime_m1000_1p_13tev",
-              #"zprime_m1250_1p_13tev", 
-              #"zprime_m1500_1p_13tev", 
-              "zprime_m2000_1p_13tev",
-              #"zprime_m3000_1p_13tev", 
-              #"zprime_m4000_1p_13tev" 
-              ]
+version = "v7"
+li_samples = [
+    "qcd_800_1000_pythia8_13tev",    
+    #"zprime_m1000_1p_13tev",
+    #"zprime_m1250_1p_13tev", 
+    #"zprime_m1500_1p_13tev", 
+    "zprime_m2000_1p_13tev",
+    #"zprime_m3000_1p_13tev", 
+    #"zprime_m4000_1p_13tev" 
+]
 
 cmssw_config_path = '/shome/gregor/TTH-73X/CMSSW/src/TTH/TTHNtupleAnalyzer/python/'
 config_script_name = 'Taggers_cfg.py'
@@ -64,7 +57,7 @@ if action == "submit":
                version,
                cmssw_config_path = cmssw_config_path,
                cmssw_config_script = config_script_name,
-               blacklist = [])
+               blacklist = ["T1_US_FNAL"])
 
 # Status
 if action == "status":
