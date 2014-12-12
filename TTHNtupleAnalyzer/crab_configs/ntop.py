dfc02e8217aace4d2b1fc6c65f8c4de1d48a1429
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-12-12 18:08:42 +0100

    Submission of v8 with extra truth content

--- TTHNtupleAnalyzer/crab_configs/ntop.py
+++ TTHNtupleAnalyzer/crab_configs/ntop.py
@@ -15,7 +15,7 @@ from TTH.TTHNtupleAnalyzer.CrabHelpers import submit, status, download, hadd
 
 # Ntuple name/version and samples to include
 name = "ntop"
-version = "v7"
+version = "v8"
 li_samples = [
     "qcd_800_1000_pythia8_13tev",    
     #"zprime_m1000_1p_13tev",
