bf11a21be8c92958cd67fd6e1af30db70a19db9d
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-12-13 11:30:52 +0100

    Playing with myScript submission

--- TTHNtupleAnalyzer/crab_configs/c_TEMPLATE.py
+++ TTHNtupleAnalyzer/crab_configs/c_TEMPLATE.py
@@ -10,12 +10,15 @@ config.General.transferLogs = True
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.allowNonProductionCMSSW = True
-config.JobType.maxJobRuntimeMin = 60*48 #maximal job runtime in minutes
+config.JobType.maxJobRuntimeMin = 60*48 #maximal job runtime in minute
+config.JobType.scriptExe = 'myScript.sh'
 
 config.section_("Data")
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.publication = False
+
+config.Data.totalUnits = 200
 
 config.section_("Site")
 
