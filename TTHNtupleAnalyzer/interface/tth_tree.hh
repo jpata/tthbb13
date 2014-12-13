2323672ab24f98fdedd4ef1de0fb7fd8b7971f2a
Joosep Pata <joosep.pata@cern.ch>
2014-12-13 21:50:49 +0200

    ran template generation for tth_tree

--- TTHNtupleAnalyzer/interface/tth_tree.hh
+++ TTHNtupleAnalyzer/interface/tth_tree.hh
@@ -8,6 +8,8 @@
 #include <cmath>
 #include <iostream>
 
+#include "TTH/TTHNtupleAnalyzer/interface/event_interpretation.hh"
+
 #define N_MAX 500
 #define M_MAX 100
 #define T_MAX 100
@@ -1555,6 +1557,74 @@ public:
 		tree->SetBranchAddress("jet_toptagger2_sj__parent_idx", jet_toptagger2_sj__parent_idx);
         //HEADERGEN_BRANCH_SETADDRESS
 
+	}
+	
+	Event* as_event() {
+		std::vector<Particle*> particles;
+		std::vector<Particle*> jets;
+		std::vector<Particle*> gen_jets;
+		std::vector<Particle*> top_decay;
+		std::vector<Particle*> higgs_decay;
+		std::vector<Particle*> leptons;
+		std::vector<Particle*> gen_leptons;
+		
+		for (int i=0;i < n__jet; i++) {
+			Particle* jet = new Particle(jet__pt[i], jet__eta[i], jet__phi[i], jet__mass[i], jet__id[i], i);
+			particles.push_back(jet);
+			jets.push_back(jet);
+		}
+		
+		for (int i=0;i < n__jet; i++) {
+			Particle* jet = new Particle(gen_jet__pt[i], gen_jet__eta[i], gen_jet__phi[i], gen_jet__mass[i], gen_jet__id[i], i);
+			particles.push_back(jet);
+			gen_jets.push_back(jet);
+		}
+		
+		for (int i=0;i < n__lep; i++) {
+			Particle* lep = new Particle(lep__pt[i], lep__eta[i], lep__phi[i], lep__mass[i], lep__id[i], i);
+			particles.push_back(lep);
+			leptons.push_back(lep);
+		}
+		
+		for (int i=0;i < n__lep; i++) {
+			Particle* lep = new Particle(gen_lep__pt[i], gen_lep__eta[i], gen_lep__phi[i], gen_lep__mass[i], gen_lep__id[i], i);
+			particles.push_back(lep);
+			gen_leptons.push_back(lep);
+		}
+		
+		Particle* d1 = new Particle(gen_t__w_d1__pt, gen_t__w_d1__eta, gen_t__w_d1__phi, gen_t__w_d1__mass, gen_t__w_d1__id);
+		top_decay.push_back(d1);
+		particles.push_back(d1);
+		
+		Particle* d2 = new Particle(gen_t__w_d2__pt, gen_t__w_d2__eta, gen_t__w_d2__phi, gen_t__w_d2__mass, gen_t__w_d2__id);
+		top_decay.push_back(d2);
+		particles.push_back(d2);
+		;
+		Particle* d3 = new Particle(gen_tbar__w_d1__pt, gen_tbar__w_d1__eta, gen_tbar__w_d1__phi, gen_tbar__w_d1__mass, gen_tbar__w_d1__id);
+		top_decay.push_back(d3);
+		particles.push_back(d3);
+		
+		Particle* d4 = new Particle(gen_tbar__w_d2__pt, gen_tbar__w_d2__eta, gen_tbar__w_d2__phi, gen_tbar__w_d2__mass, gen_tbar__w_d2__id);
+		top_decay.push_back(d4);
+		particles.push_back(d4);
+		
+		Particle* d5 = new Particle(gen_t__b__pt, gen_t__b__eta, gen_t__b__phi, gen_t__b__mass, 5);
+		top_decay.push_back(d5);
+		particles.push_back(d5);
+		
+		Particle* d6 = new Particle(gen_tbar__b__pt, gen_tbar__b__eta, gen_tbar__b__phi, gen_tbar__b__mass, -5);
+		top_decay.push_back(d6);
+		particles.push_back(d6);
+		
+		Particle* d7 = new Particle(gen_b__pt, gen_b__eta, gen_b__phi, gen_b__mass, 5);
+		higgs_decay.push_back(d7);
+		particles.push_back(d7);
+		
+		Particle* d8 = new Particle(gen_bbar__pt, gen_bbar__eta, gen_bbar__phi, gen_bbar__mass, -5);
+		higgs_decay.push_back(d8);
+		particles.push_back(d8);
+		
+		return new Event(particles, jets, gen_jets, leptons, gen_leptons, top_decay, higgs_decay);
 	}
 };
 
