2dc92d55d9a979873ce2053e441f247dcd229b6b
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-12-15 14:08:32 +0100

    Playing with submission for myScript

--- TTHNtupleAnalyzer/crab_configs/myScript.sh
+++ TTHNtupleAnalyzer/crab_configs/myScript.sh
@@ -1,5 +1,24 @@
 echo "Hello World!"
 
+echo "ls -lactrh"
+ls -lactrh
+
+echo "ls -lactrh *"
+ls -lactrh *
+
+echo "pwd"
+pwd
+
+
+echo "LD_LIBRARY_PATH="
+echo $LD_LIBRARY_PATH   
+
+echo "export"
+export LD_LIBRARY_PATH=$PWD/lib/slc6_amd64_gcc491/:$LD_LIBRARY_PATH
+
+echo "LD_LIBRARY_PATH="
+echo $LD_LIBRARY_PATH   
+
 # Actually, let's do something more useful than a simple hello world... this will print the input arguments passed to the script
 echo "Here there are all the input arguments"
 echo $@
@@ -14,3 +33,5 @@ python -c "import PSet; print PSet.process.dumpPython()"
 
 # Ok, let's stop fooling around and execute the job:
 cmsRun -j FrameworkJobReport.xml -p PSet.py
+
+echo "Done"
