4234f39e41fef347979a595af2a8550d1d3d453a
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-12-15 19:26:55 +0100

    Correlation Plotting

---
+++ Plotting/python/gregor/Correlations.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+"""
+"""
+
+########################################
+# Imports
+########################################
+
+from TTH.Plotting.Helpers.CorrelationHelpers import *
+
+
+########################################
+# Define Input Files and
+# output directory
+########################################
+
+basepath = '/scratch/gregor/'
+
+files = {}
+files["qcd_800_1000"] = "ntop_v8_qcd_800_1000_pythia8_13tev-tagging"
+files["zprime_m2000_1p"] = "ntop_v8_zprime_m2000_1p_13tev-tagging"
+                                        
+# for the filename: basepath + filename + .root
+for k,v in files.iteritems():
+    files[k] = basepath + v + ".root"
+
+
+mass_vars = [
+    var('ca15_mass', "m", 0, 1000),
+    var('ca15filtered_mass', "filtered m", 0, 1000),
+    var('ca15pruned_mass', "pruned m", 0, 1000),
+    var('ca15trimmed_mass', "trimmed m", 0, 1000),
+    var('ca15softdrop_mass', "softdrop m", 0, 1000),    
+]
+
+tau_vars = [
+    var('ca15_tau3/ca15_tau2', "#tau_{3}/#tau_{2}", 0, 2.),
+    var('ca15filtered_tau3/ca15filtered_tau2', "filtered #tau_{3}/#tau_{2}", 0, 2.),
+    var('ca15pruned_tau3/ca15pruned_tau2', "pruned #tau_{3}/#tau_{2}", 0, 2.),
+    var('ca15trimmed_tau3/ca15trimmed_tau2', "trimmed #tau_{3}/#tau_{2}", 0, 2.),
+    var('ca15softdrop_tau3/ca15softdrop_tau2', "softdrop #tau_{3}/#tau_{2}", 0, 2.),    
+]
+
+tagger_vars = [
+    var('looseMultiRHTT_mass', "HTT m", 0, 1000),
+    var('looseMultiRHTT_fW', "HTT f_{W}", 0, 2),
+    var('looseMultiRHTT_Rmin-looseMultiRHTT_RminExpected', "HTT #Delta R_{min,exp}", -3., 3.),
+    var('ca15cmstt_minMass', "CMSTT minMass", 0., 1000),
+]
+
+                                                                              
+#'jet_ca15cmstt':"CMSTT",             
+#             "jet_looseMultiRHTT" : "HTT",}
+
+
+#corrs = [corr("masses", "zprime_m2000_1p", mass_vars)]
+#corrs = [corr("taus", "zprime_m2000_1p", tau_vars)]
+#corrs = [corr("taggers", "zprime_m2000_1p", tagger_vars)]
+#corrs = [corr("all", "zprime_m2000_1p", mass_vars+tau_vars+tagger_vars)]
+
+corrs = [corr("all", "qcd_800_1000", mass_vars+tau_vars+tagger_vars)]
+
+
+MakePlots(corrs, files)
--- Plotting/python/gregor/Test2d.py
+++ Plotting/python/gregor/Test2d.py
@@ -17,8 +17,9 @@ from TTH.Plotting.Helpers.Plot2DHelpers import *
 basepath = '/scratch/gregor/'
 
 files = {}
-files["ntop_v8_zprime_m2000_1p_13tev-tagging"       ] = "ntop_v8_zprime_m2000_1p_13tev-tagging"
-                                         
+files["qcd_800_1000"] = "ntop_v8_qcd_800_1000_pythia8_13tev-tagging"
+files["zprime_m2000_1p"] = "ntop_v8_zprime_m2000_1p_13tev-tagging"
+                                        
 # for the filename: basepath + filename + .root
 for k,v in files.iteritems():
     # only a filename
@@ -31,14 +32,49 @@ for k,v in files.iteritems():
 
 
 plots = []
-plots.append( plot( 
-    "name", 
-    'ca15_mass', 
-    'ca15trimmed_mass', 
-    '(1)', 
-    "ntop_v8_zprime_m2000_1p_13tev-tagging",
-    50, 0, 800,
-    50, 0, 800))
-    
+
+for sample in files.keys(): 
+
+
+    plots.append( plot( 
+        "ca15_tau32_vs_ca15_trimmed_tau32_{0}".format(sample), 
+        'ca15_tau3/ca15_tau2', 
+        'ca15trimmed_tau3/ca15trimmed_tau2', 
+        '(1)', 
+        sample, 
+        60, 0, 1.2,
+        60, 0, 1.2,
+        label_x = "CA (R=1.5) #tau_{3}/#tau_{2}",
+        label_y = "CA (R=1.5) trimmed #tau_{3}/#tau_{2}",
+    ))
+
+
+
+    if False:
+        plots.append( plot( 
+            "ca15_mass_vs_ca08_mass_{0}".format(sample), 
+            'ca15_mass', 
+            'ca08_mass', 
+            '(1)', 
+            sample, 
+            50, 0, 800,
+            50, 0, 800,
+            label_x = "CA (R=1.5) mass [GeV]",
+            label_y = "CA (R=0.8) mass [GeV]",
+        ))
+
+
+        plots.append( plot( 
+            "ca15_mass_vs_ca15_trimmed_mass_{0}".format(sample), 
+            'ca15_mass', 
+            'ca15trimmed_mass', 
+            '(1)', 
+            sample, 
+            50, 0, 800,
+            50, 0, 800,
+            label_x = "CA (R=1.5) mass [GeV]",
+            label_y = "CA (R=1.5) trimmed mass [GeV]",
+        ))
+   
 
 MakePlots(files, plots )
