feed92bfb2ec9f800ee7cf8afaca72920df8f1a5
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2014-12-18 14:49:22 +0100

    Updated ROC plotting

--- Plotting/python/Helpers/TMVAHelpers.py
+++ Plotting/python/Helpers/TMVAHelpers.py
@@ -10,56 +10,123 @@ Helper script for the evaluation of different methods/variables using TMVA.
 import sys
 import pickle
 
-from ROOT import TFile, TTree, TCut, TMVA
+import ROOT
 
+# initializer: simple creation of bag-of-object classes
+from Initializer import initializer
+
+
+########################################
+# class variable
+########################################
+
+class variable:
+    """ Helper class to store a variable used as TMVA input """
+
+    @initializer
+    def __init__(self,
+                 name,
+                 pretty_name = "",
+                 allowed_range = [],
+                 extra_cut = ""):
+        if pretty_name == "":
+            pretty_name = name
+
+# end of variable class
+
+
+########################################
+# class TMVASetup
+########################################
+
+class TMVASetup:
+    """Helper class to store options for running TMVA.
+
+    - Each variable can have an allowed range as well as an extra cut
+    - We need to record the efficiency of these cuts with respect to the ficucial selection
+        -> N(fiducual && per-variable-cuts) / N(fiducial)
+    - These numbers are written to a pickle file so the ROC curves can be correctly     
+    - Then all the TMVA methods are run on the set of variables
+
+    Arguments:
+    name             - [string]: will be used for naming the output files
+    pretty_name      - [string]: used for labelling
+    li_methods       - [list of string objects] TMVA methods to apply
+    li_vars          - [list of variable objects] variables to include
+    file_name_sig    - [string]: full name of the input signal file
+    file_name_bg     - [string]: full name of the input background file
+    tree_name_sig    - [string]: name of the tree with signal events
+    tree_name_bg     - [string]: name of the tree with background events
+    fiducial_cut_sig - [string]: fiducial cut signal - define denominator of efficiencies
+    fiducial_cut_bg  - [string]: fiducial cut background - define denominator of efficiencies
+    """
+
+    @initializer    
+    def __init__( self, 
+                  name,
+                  pretty_name,
+                  li_methods,
+                  li_vars,
+                  file_name_sig,
+                  file_name_bg,
+                  tree_name_sig    = "tree",
+                  tree_name_bg     = "tree",
+                  fiducial_cut_sig = "(1)",
+                  fiducial_cut_bg  = "(1)",
+              ):
+        pass
+
+# end of TMVASetup class
 
 
 ########################################
 # doTMVA
 ########################################
 
-def doTMVA( infnameSig, infnameBkg,
-            outfname,
-            li_methods,
-            li_vars,
-            treeNameSig  = "tree",
-            treeNameBkg  = "tree",
-        ):
+def doTMVA(setup):
         
-    ########################################
-    # Prepare IO & TMVA
-    ########################################
-
     # Open Files
-    outputFile    = TFile( outfname, 'RECREATE' )
+    # Output
+    outfname = "TMVA_{0}.root".format(setup.name)
+    outputFile    = ROOT.TFile( outfname, 'RECREATE' )
     outputFilePickle = open( outfname.replace(".root",".dat"), "w")
-    inputFileSig  = TFile.Open( infnameSig )
-    inputFileBkg  = TFile.Open( infnameBkg )
 
-    # Create instance of TMVA factory
-    factory = TMVA.Factory( "TMVAClassification", outputFile, 
-                            "!V:!Silent:Color:DrawProgressBar:Transformations=I:AnalysisType=Classification" )
-    factory.SetVerbose( False )
+    # Input
+    inputFileSig  = ROOT.TFile.Open( setup.file_name_sig )
+    inputFileBkg  = ROOT.TFile.Open( setup.file_name_bg )
 
-    # Add variables to factory
-    li_cuts = ["(1)"]
-    for var in li_vars:
-        factory.AddVariable( var[0], 'F' )
-        li_cuts.append( "({0}>={1})".format(var[0],var[1]))
-        li_cuts.append( "({0}<={1})".format(var[0],var[2]))
-
-    # Apply additional cuts on the signal and background sample. 
-    cut = "&&".join(li_cuts)
-    
     # Get the signal and background trees for training
-    signal      = inputFileSig.Get( treeNameSig )
-    background  = inputFileBkg.Get( treeNameBkg )
+    signal      = inputFileSig.Get( setup.tree_name_sig )
+    background  = inputFileBkg.Get( setup.tree_name_bg )
 
-    n_sig = signal.Draw("(1)","(1)")
-    n_sig_cut = signal.Draw("(1)", cut)
 
-    n_bg = background.Draw("(1)","(1)")
-    n_bg_cut = background.Draw("(1)", cut)
+    ########################################
+    # Create cuts and calculate efficiency
+    ########################################
+
+    # Combine all the cuts associated to variables
+    li_cuts = ["(1)"]
+    for var in setup.li_vars:        
+        if var.extra_cut:
+            li_cuts.append(var.extra_cut)
+            
+        if len(var.allowed_range):                    
+            li_cuts.append( "({0}>={1})".format(var.name, var.allowed_range[0]))
+            li_cuts.append( "({0}<={1})".format(var.name, var.allowed_range[1]))
+
+    # The final cut is:
+    #  - the && of per-variable cuts 
+    #  - && the per-sample fidcucial cuts
+    cut_signal = "&&".join(li_cuts + [setup.fiducial_cut_sig])
+    cut_bg     = "&&".join(li_cuts + [setup.fiducial_cut_bg])
+
+    # Calculate cut efficiency and store it in a pickle file
+    # This can be used later to properly normalize the ROC curves
+    n_sig = signal.Draw("(1)", setup.fiducial_cut_sig)
+    n_sig_cut = signal.Draw("(1)", cut_signal)
+
+    n_bg = background.Draw("(1)", setup.fiducial_cut_bg)
+    n_bg_cut = background.Draw("(1)", cut_bg)
 
     cut_frac = {}
     cut_frac["sig"] = (1. * n_sig_cut / n_sig)
@@ -67,6 +134,20 @@ def doTMVA( infnameSig, infnameBkg,
     
     pickle.dump(cut_frac, outputFilePickle)
     outputFilePickle.close()
+
+
+    ########################################
+    # TMVA starts here
+    ########################################
+
+    # Create instance of TMVA factory
+    factory = ROOT.TMVA.Factory( "TMVAClassification", outputFile, 
+                            "!V:!Silent:Color:DrawProgressBar:Transformations=I:AnalysisType=Classification" )
+    factory.SetVerbose( False )
+
+    # Add variables to factory
+    for var in setup.li_vars:
+        factory.AddVariable( var.name, 'F' )
 
     # Global event weights
     signalWeight     = 1.0
@@ -76,9 +157,8 @@ def doTMVA( infnameSig, infnameBkg,
     factory.AddSignalTree    ( signal,     signalWeight     )
     factory.AddBackgroundTree( background, backgroundWeight )
 
-
-    mycutSig = TCut( cut ) 
-    mycutBkg = TCut( cut ) 
+    mycutSig = ROOT.TCut(cut_signal ) 
+    mycutBkg = ROOT.TCut(cut_bg ) 
 
     # prepare trees
     factory.PrepareTrainingAndTestTree( mycutSig, mycutBkg,
@@ -89,49 +169,48 @@ def doTMVA( infnameSig, infnameBkg,
     # Schedule the Classifiers
     ########################################
     
-    # Cut optimisation
-    if "Cuts" in li_methods:
-        factory.BookMethod( TMVA.Types.kCuts, 
+    if "Cuts" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kCuts, 
                             "Cuts",
                             "FitMethod=SA" )
 
-    if "Likelihood" in li_methods:
-        factory.BookMethod( TMVA.Types.kLikelihood, 
+    if "Likelihood" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kLikelihood, 
                             "Likelihood",
                             "" )
 
-    if "Fisher" in li_methods:
-        factory.BookMethod( TMVA.Types.kFisher, 
+    if "Fisher" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kFisher, 
                             "Fisher", 
                             "H:!V:Fisher:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10" )
 
-    if "MLP" in li_methods:
-        factory.BookMethod( TMVA.Types.kMLP, 
+    if "MLP" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kMLP, 
                             "MLP", 
                             "H:!V:NeuronType=tanh:VarTransform=N:NCycles=1200:HiddenLayers=N+5:TestRate=5:!UseRegulator" )
 
-    if "SVM" in li_methods:
-        factory.BookMethod( TMVA.Types.kSVM, 
+    if "SVM" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kSVM, 
                             "SVM", 
                             "Gamma=0.25:Tol=0.001:VarTransform=Norm" )
 
-    if "BDTG" in li_methods:
-        factory.BookMethod( TMVA.Types.kBDT, 
+    if "BDTG" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kBDT, 
                             "BDTG",
         "!H:!V:NTrees=1000:BoostType=Grad:Shrinkage=0.30:UseBaggedGrad:GradBaggingFraction=0.6:SeparationType=GiniIndex:nCuts=20:NNodesMax=5" )
 
-    if "BDT" in li_methods:
-        factory.BookMethod( TMVA.Types.kBDT, 
+    if "BDT" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kBDT, 
                             "BDT",
                             "!H:!V:NTrees=850:nEventsMin=150:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:SeparationType=GiniIndex:nCuts=20:PruneMethod=NoPruning" )
 
-    if "BDTB" in li_methods:
-        factory.BookMethod( TMVA.Types.kBDT, 
+    if "BDTB" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kBDT, 
                             "BDTB",
                             "!H:!V:NTrees=400:BoostType=Bagging:SeparationType=GiniIndex:nCuts=20:PruneMethod=NoPruning" )
 
-    if "BDTD" in li_methods:
-        factory.BookMethod( TMVA.Types.kBDT, 
+    if "BDTD" in setup.li_methods:
+        factory.BookMethod( ROOT.TMVA.Types.kBDT, 
                             "BDTD",
                             "!H:!V:NTrees=400:nEventsMin=400:MaxDepth=3:BoostType=AdaBoost:SeparationType=GiniIndex:nCuts=20:PruneMethod=NoPruning:VarTransform=Decorrelate" )
 
@@ -144,5 +223,100 @@ def doTMVA( infnameSig, infnameBkg,
     factory.TestAllMethods()
     factory.EvaluateAllMethods()    
     outputFile.Close()
-
 # end of doTMVA
+
+
+########################################
+# plotROCs
+########################################
+
+def plotROCs(name, li_setups):
+    
+    li_colors = [ROOT.kRed,      ROOT.kBlue+1,     ROOT.kBlack, 
+                 ROOT.kOrange-1, ROOT.kViolet+1,   ROOT.kGreen+1,
+                 ROOT.kGray,     ROOT.kYellow]*10
+
+    li_line_styles = [1,2]*40
+
+
+    ########################################
+    # Extract histograms from files
+    ########################################
+
+    li_hs    = []
+    li_names = []
+    li_pretty_names = []
+
+    di_cut_fracs = {}
+
+    for setup in li_setups: 
+        input_filename = "TMVA_{0}.root".format(setup.name)
+        f = ROOT.TFile(input_filename, "READ" )
+
+        for method in setup.li_methods:
+            h = f.FindObjectAny("MVA_" + method + "_rejBvsS")
+            hc = h.Clone()
+            hc.SetDirectory(0)
+            li_hs.append( hc )
+            li_names.append( setup.name  )
+            li_pretty_names.append(setup.pretty_name)
+
+        f_pickle = open( input_filename.replace(".root",".dat"), "r")
+        di_cut_fracs[setup.name] = pickle.load(f_pickle)        
+    # end loop over infiles
+
+
+    ########################################
+    # And draw them
+    ########################################
+
+    c = ROOT.TCanvas( "","", 800, 800)
+
+    legend_origin_x     = 0.17
+    legend_origin_y     = 0.18
+    legend_size_x       = 0.3
+    legend_size_y       = 0.03 * len(li_hs)
+
+    legend = ROOT.TLegend( legend_origin_x, 
+                           legend_origin_y,
+                           legend_origin_x + legend_size_x,
+                           legend_origin_y + legend_size_y )
+    legend.SetBorderSize(1) 
+    legend.SetFillColor(0)
+    legend.SetTextSize(0.03)      
+    legend.SetBorderSize(0)
+
+    h_bg = ROOT.TH2F("","",100,0,1,100,0,1)
+
+    h_bg.GetXaxis().SetTitle( "#varepsilon(S)" )      
+    h_bg.GetYaxis().SetTitle( "1-#varepsilon(B)" )      
+    h_bg.Draw()
+
+    for i_h, h in enumerate(li_hs):
+
+        cut_fraction_signal = di_cut_fracs[li_names[i_h]]["sig"] 
+        cut_fraction_bg = di_cut_fracs[li_names[i_h]]["bg"] 
+
+        print li_names[i_h], cut_fraction_signal, cut_fraction_bg
+
+        h.GetXaxis().SetLimits(0.0,cut_fraction_signal)
+
+        for ibin in range(1, h.GetXaxis().GetNbins()):
+            eff_b = 1 - h.GetBinContent(ibin)
+            eff_b_new = eff_b * cut_fraction_bg
+            h.SetBinContent(ibin, 1-eff_b_new)
+
+        h.SetLineColor( li_colors[i_h] )
+        h.SetLineStyle( li_line_styles[i_h] )
+        h.SetLineWidth( 2)    
+        legend.AddEntry( h, li_pretty_names[i_h], "L" )
+
+    for h in li_hs:    
+        h.Draw("SAME")
+    legend.Draw()
+
+    c.Print(name+".png")
+# end of plotROCs
+
+
+
--- Plotting/python/gregor/ClassifyTaggers.py
+++ Plotting/python/gregor/ClassifyTaggers.py
@@ -9,40 +9,13 @@ Schedule the testing of different variable/method-sets with TMVA
 
 import pickle
 
-import ROOT
-
-from TTH.Plotting.Helpers.TMVAHelpers import doTMVA
+from TTH.Plotting.Helpers.TMVAHelpers import variable, TMVASetup, doTMVA, plotROCs
 from TTH.Plotting.Helpers.PrepareRootStyle import myStyle
 
-
-
+import ROOT
 
 ROOT.gROOT.SetStyle("myStyle")
 ROOT.gROOT.ForceStyle()
-
-
-########################################
-# Helper Class to store: 
-#  -variables
-#  -methods
-#  -name
-########################################
-
-class TMVASetup:
-    def __init__( self, 
-                  name,
-                  li_methods,
-                  li_vars,
-                  treeNameSig  = "tree",
-                  treeNameBkg  = "tree",
-              ):
-        self.name        = "TMVA_" + name + ".root"
-        self.li_methods  = li_methods
-        self.li_vars     = li_vars
-        self.treeNameSig = treeNameSig
-        self.treeNameBkg = treeNameBkg
-    # end of init
-# end of TMVASetup class
 
 
 ########################################
@@ -52,140 +25,98 @@ class TMVASetup:
 run_TMVA = False
 
 basepath = '/scratch/gregor/'
-infnameSig   = "ntop_v8_zprime_m2000_1p_13tev-tagging.root"
-infnameBkg   = "ntop_v8_qcd_800_1000_pythia8_13tev-tagging.root"
+file_name_sig  = basepath + "ntop_v8_zprime_m2000_1p_13tev-tagging.root"
+file_name_bg   = basepath + "ntop_v8_qcd_800_1000_pythia8_13tev-tagging.root"
 
-# Common options
 li_methods      = ["Likelihood"]
 
-li_TMVAs = []
+# We want to make single-variable ROC curves
+# so first create a list of variables and then send them individually to TMVA
+
+# Masses
+mass_vars_15 = [
+    variable("ca15_mass", "m (R=1.5)", allowed_range=[0,2000]),
+    variable("ca15filtered_mass", "filtered m (R=1.5)", allowed_range=[0,2000]),
+    variable("ca15pruned_mass", "pruned m (R=1.5)", allowed_range=[0,2000]),
+    variable("ca15trimmed_mass", "trimmed m (R=1.5)", allowed_range=[0,2000]),
+    variable("ca15softdrop_mass", "softdrop m (R=1.5)", allowed_range=[0,2000]),
+]
+
+mass_vars_08 = [
+    variable("ca08_mass", "m (R=0.8)", allowed_range=[0,2000]),
+    variable("ca08filtered_mass", "filtered m (R=0.8)", allowed_range=[0,2000]),
+    variable("ca08pruned_mass", "pruned m (R=0.8)", allowed_range=[0,2000]),
+    variable("ca08trimmed_mass", "trimmed m (R=0.8)", allowed_range=[0,2000]),
+    variable("ca08softdrop_mass", "softdrop m (R=0.8)", allowed_range=[0,2000]),
+]
+
+# N-Subjettiness
+tau_vars_15 = [
+    variable("ca15_tau3/ca15_tau2", "#tau_{3}/#tau_{2} (R=1.5)", allowed_range=[0,2]),
+    variable("ca15filtered_tau3/ca15filtered_tau2", "filtered #tau_{3}/#tau_{2} (R=1.5)", allowed_range=[0,2]),
+    variable("ca15pruned_tau3/ca15pruned_tau2", "pruned #tau_{3}/#tau_{2} (R=1.5)", allowed_range=[0,2]),
+    variable("ca15trimmed_tau3/ca15trimmed_tau2", "trimmed #tau_{3}/#tau_{2} (R=1.5)", allowed_range=[0,2]),
+    variable("ca15softdrop_tau3/ca15softdrop_tau2", "softdrop #tau_{3}/#tau_{2} (R=1.5)", allowed_range=[0,2]),
+]
+
+tau_vars_08 = [
+    variable("ca08_tau3/ca08_tau2", "#tau_{3}/#tau_{2} (R=0.8)", allowed_range=[0,2]),
+    variable("ca08filtered_tau3/ca08filtered_tau2", "filtered #tau_{3}/#tau_{2} (R=0.8)", allowed_range=[0,2]),
+    variable("ca08pruned_tau3/ca08pruned_tau2", "pruned #tau_{3}/#tau_{2} (R=0.8)", allowed_range=[0,2]),
+    variable("ca08trimmed_tau3/ca08trimmed_tau2", "trimmed #tau_{3}/#tau_{2} (R=0.8)", allowed_range=[0,2]),
+    variable("ca08softdrop_tau3/ca08softdrop_tau2", "softdrop #tau_{3}/#tau_{2} (R=0.8)", allowed_range=[0,2]),
+]
+
+# Taggers
+tagger_vars_15 = [
+    variable("ca15cmstt_minMass", "CMSTT minMass (R=1.5)", allowed_range=[0,2000]),
+    variable("looseMultiRHTT_mass", "HTT mass", allowed_range=[0,400]),
+    variable("looseMultiRHTT_fW", "HTT f_{W}", allowed_range=[0,2]),
+    variable("looseMultiRHTT_Rmin-looseMultiRHTT_RminExpected", "#Delta R_{min,exp}", allowed_range=[-3,3]),
+]
+
+tagger_vars_08 = [
+    variable("ca08cmstt_minMass", "CMSTT minMass (R=0.8)", allowed_range=[0,2000]),
+]
+
+all_vars_15 = mass_vars_15 + tau_vars_15 + tagger_vars_15
+all_vars_08 = mass_vars_08 + tau_vars_08 + tagger_vars_08
+
+def create_setups(li_vars):
+    li_TMVAs = []
+    for v in li_vars:
+        name = v.name.replace("/","_")
+        li_TMVAs.append( TMVASetup( name,
+                                    v.pretty_name,
+                                    li_methods, 
+                                    [v],
+                                    file_name_sig,
+                                    file_name_bg))
+    return li_TMVAs
+# end of create_setups
+    
+mass_setups_15 = create_setups(mass_vars_15)
+tau_setups_15  = create_setups(tau_vars_15)
+tagger_setups_15 = create_setups(tagger_vars_15)
+all_setups_15 = create_setups(all_vars_15)
+
+mass_setups_08 = create_setups(mass_vars_08)
+tau_setups_08  = create_setups(tau_vars_08)
+tagger_setups_08 = create_setups(tagger_vars_08)
+all_setups_08 = create_setups(all_vars_08)
 
 
-#li_TMVAs.append( TMVASetup( "m",  li_methods, [[ "ca15_mass",0,2000]]))
-#li_TMVAs.append( TMVASetup( "filtered m",  li_methods, [[ "ca15filtered_mass",0,2000]]))
-#li_TMVAs.append( TMVASetup( "pruned m",  li_methods, [[ "ca15pruned_mass",0,2000]]))
-#li_TMVAs.append( TMVASetup( "trimmed_m",  li_methods, [[ "ca15trimmed_mass",0,2000]]))
-#li_TMVAs.append( TMVASetup( "softdrop m",  li_methods, [[ "ca15softdrop_mass",0,2000]]))
+mass_setups = mass_setups_08 + mass_setups_15
+tau_setups = tau_setups_08 + tau_setups_15
+tagger_setups = tagger_setups_08 + tagger_setups_15
 
-#li_TMVAs.append( TMVASetup( "#tau_{3}/#tau_{2}",  li_methods, [[ "ca15_tau3/ca15_tau2",0,2]]))
-#li_TMVAs.append( TMVASetup( "filtered #tau_{3}/#tau_{2}",  li_methods, [[ "ca15filtered_tau3/ca15filtered_tau2",0,2]]))
-#li_TMVAs.append( TMVASetup( "pruned #tau_{3}/#tau_{2}",  li_methods, [[ "ca15pruned_tau3/ca15pruned_tau2",0,2]]))
-#li_TMVAs.append( TMVASetup( "trimmed #tau_{3}/#tau_{2}",  li_methods,[[ "ca15trimmed_tau3/ca15trimmed_tau2",0,2]]))
-#li_TMVAs.append( TMVASetup( "softdrop #tau_{3}/#tau_{2}",  li_methods, [[ "ca15softdrop_tau3/ca15softdrop_tau2",0,2]]))
-
-
-li_TMVAs.append( TMVASetup( "CMSS minMass",  li_methods,[["ca15cmstt_minMass", 0, 2000]]))
-li_TMVAs.append( TMVASetup( "HTT m",  li_methods, [ ['looseMultiRHTT_mass', 0, 2000]]))
-li_TMVAs.append( TMVASetup( "HTT fw",  li_methods, [[ "looseMultiRHTT_fW", 0, 1.0]]))
-li_TMVAs.append( TMVASetup( "HTT #Delta R_{min,exp}",  li_methods, [[ "looseMultiRHTT_Rmin-looseMultiRHTT_RminExpected", -3, 3]]))
-
-
-########################################
-# doTMVA
-########################################
 
 if run_TMVA:
-    for TMVAops in li_TMVAs:
-        doTMVA( basepath + infnameSig, 
-                basepath + infnameBkg,
-                TMVAops.name,
-                TMVAops.li_methods,
-                TMVAops.li_vars,
-                treeNameSig  = TMVAops.treeNameSig,
-                treeNameBkg  = TMVAops.treeNameBkg,
-        )
-        
+    for setup in all_setups_08:
+        doTMVA(setup)
 
-li_colors = [ROOT.kRed,      ROOT.kBlue+1,     ROOT.kBlack, 
-             ROOT.kOrange-1, ROOT.kViolet+1,   ROOT.kGreen+1,
-             ROOT.kGray,     ROOT.kYellow,
-         ]*10
-
-
-########################################
-# Extract histograms from files
-########################################
-
-li_hs    = []
-li_names = []
-
-di_cut_fracs = {}
-
-for TMVAops in li_TMVAs:    
-    f = ROOT.TFile( TMVAops.name, "READ" )
-    
-    for method in li_methods:
-        h = f.FindObjectAny("MVA_" + method + "_rejBvsS")
-        hc = h.Clone()
-        hc.SetDirectory(0)
-        li_hs.append( hc )
-        name = TMVAops.name.replace(".root","").replace("TMVA_","")
-        li_names.append( name  )
-
-    f_pickle = open( TMVAops.name.replace(".root",".dat"), "r")
-    di_cut_fracs[name] = pickle.load(f_pickle)
-    
-        
-# end loop over infiles
-
-
-########################################
-# And draw them
-########################################
-
-c = ROOT.TCanvas( "","", 800, 800)
-
-legend_origin_x     = 0.2
-legend_origin_y     = 0.25
-legend_size_x       = 0.3
-legend_size_y       = 0.045 * len(li_hs)
-
-legend = ROOT.TLegend( legend_origin_x, 
-                       legend_origin_y,
-                       legend_origin_x + legend_size_x,
-                       legend_origin_y + legend_size_y )
-legend.SetBorderSize(1) 
-legend.SetFillColor(0)
-legend.SetTextSize(0.04)      
-legend.SetBorderSize(0)
-
-
-h_bg = ROOT.TH2F("","",100,0,1,100,0,1)
-
-h_bg.GetXaxis().SetTitle( "#varepsilon(S)" )      
-h_bg.GetYaxis().SetTitle( "1-#varepsilon(B)" )      
-h_bg.Draw()
-
-
-
-for i_h, h in enumerate(li_hs):
-
-    cut_fraction_signal = di_cut_fracs[li_names[i_h]]["sig"] 
-    cut_fraction_bg = di_cut_fracs[li_names[i_h]]["bg"] 
-
-    print li_names[i_h], cut_fraction_signal, cut_fraction_bg
-
-    h.GetXaxis().SetLimits(0.0,cut_fraction_signal)
-
-    for ibin in range(1, h.GetXaxis().GetNbins()):
-        eff_b = 1 - h.GetBinContent(ibin)
-        eff_b_new = eff_b * cut_fraction_bg
-        h.SetBinContent(ibin, 1-eff_b_new)
-
-
-    h.SetLineColor( li_colors[i_h] )
-    h.SetLineWidth( 2)    
-    legend.AddEntry( h, li_names[i_h], "L" )
-
-
-for h in li_hs:    
-    h.Draw("SAME")
-legend.Draw()
-
-#line  = ROOT.TLine()
-#line.SetLineColor( ROOT.kBlack )
-#line.SetLineWidth( 2 )
-#line.DrawLine( 0, 1, 1, 0 )
-
-c.Print("ROC.png")
-
+plotROCs("ROC_mass", mass_setups)
+plotROCs("ROC_tau", tau_setups)
+plotROCs("ROC_tagger", tagger_setups)
+plotROCs("ROC_all_08", all_setups_08)
+plotROCs("ROC_all_15", all_setups_15)
