607e40d407497c647b9efb5486d4305c9751f5f9
Gregor Kasieczka <g.kasieczka@gmail.com>
2015-01-13 11:18:00 +0100

    Made Mutual Information for two variables work

--- Plotting/python/Helpers/MutualInformationHelpers.py
+++ Plotting/python/Helpers/MutualInformationHelpers.py
@@ -4,7 +4,7 @@ Helpers to calculate mutual information.
 
 Inspired by 1408.3122
 """
-
+ 
 ########################################
 # Imports and setup ROOT with style
 ########################################
@@ -12,7 +12,10 @@ Inspired by 1408.3122
 import math
 import glob
 import copy
+import sys
+
 import ROOT
+
 from TTH.Plotting.python.Helpers.PrepareRootStyle import myStyle
 
 ROOT.gStyle.SetPadLeftMargin(0.2)
@@ -42,8 +45,10 @@ ROOT.gStyle.SetPaintTextFormat("3.2f");
 
 ROOT.TH1.SetDefaultSumw2()
 
-
 ROOT.gErrorIgnoreLevel = 1
+
+# Global variable to count drawn histograms for unique naming
+i_draw = 0 
 
 ########################################
 # Import private support code
@@ -99,17 +104,114 @@ def CalculateEntropy(h, verbose = False):
    total = h.Integral()
    checksum = 0
    entropy = 0.
-   for i_bin in range(1, h.GetXaxis().GetNbins()+1):
-      width = h.GetBinWidth(i_bin)
-      p = 1. * h.Integral(i_bin, i_bin)/total
-      if p>0:
-         checksum += p
-         entropy -= p*math.log(p,2) 
-         
+
+   # 1d Histograms
+   if type(h) in [ROOT.TH1F, ROOT.TH1D]:
+      for i_bin in range(1, h.GetXaxis().GetNbins()+1):
+
+         p = 1. * h.GetBinContent(i_bin)/total
+         if p>0:
+            checksum += p
+            entropy -= p*math.log(p,2) 
+
+   # 2d Histograms
+   elif type(h) in [ROOT.TH2F, ROOT.TH2D]:
+      
+      for i_bin_x in range(1, h.GetXaxis().GetNbins()+1):
+         for i_bin_y in range(1, h.GetYaxis().GetNbins()+1):
+            
+            p = 1. * h.GetBinContent(i_bin_x, i_bin_y)/total
+            if p>0:
+               checksum += p
+               entropy -= p*math.log(p,2) 
+
+   # 3d Histograms
+   elif type(h) in [ROOT.TH3F, ROOT.TH3D]:
+      
+      for i_bin_x in range(1, h.GetXaxis().GetNbins()+1):
+         for i_bin_y in range(1, h.GetYaxis().GetNbins()+1):
+            for i_bin_z in range(1, h.GetZaxis().GetNbins()+1):
+            
+               p = 1. * h.GetBinContent(i_bin_x, i_bin_y, i_bin_z)/total
+               if p>0:
+                  checksum += p
+                  entropy -= p*math.log(p,2) 
+
+   # Invalid Histogram type
+   else:
+      print "Ivalid type {0} for histogram in CalculateEntropy".format(type(h))
+      print "Exiting"
+   # End of handling different histogram dimensions
+
    if verbose:
       print "checksum = ", checksum
+
    return entropy
+
 # End of CalculateEntropy      
+
+
+########################################
+# MakeHistogram
+########################################
+
+def MakeHistogram(tree, variables, n_bins, cuts):
+   """ Produce a histgrom for given variables, number of bins per axis
+   and cuts from a ROOT tree"""
+
+   global i_draw
+
+   tmp_name = "htmp{0}".format(i_draw)
+   i_draw += 1
+
+   # Make a TH1
+   if len(variables)==1:
+      draw_string = "{0}>>{1}({2},{3},{4})".format(variables[0].name, 
+                                                   tmp_name,
+                                                   n_bins,
+                                                   variables[0].range_min,
+                                                   variables[0].range_max)
+
+   # Make a TH2
+   elif len(variables)==2:
+      draw_string = "{0}:{1}>>{2}({3},{4},{5},{6},{7},{8})".format(variables[1].name,  # y
+                                                                   variables[0].name,  # x
+                                                                   tmp_name,
+                                                                   n_bins,
+                                                                   variables[0].range_min,
+                                                                   variables[0].range_max,
+                                                                   n_bins,
+                                                                   variables[1].range_min,
+                                                                   variables[1].range_max)
+
+   # Make a TH3
+   elif len(variables)==3:
+      draw_string = "{0}:{1}:{2}>>{3}({4},{5},{6},{7},{8},{9},{10},{11},{12})".format(variables[2].name,  # z
+                                                                                      variables[1].name,  # y
+                                                                                      variables[0].name,  # x
+                                                                                      tmp_name,
+                                                                                      n_bins,
+                                                                                      variables[0].range_min,
+                                                                                      variables[0].range_max,
+                                                                                      n_bins,
+                                                                                      variables[1].range_min,
+                                                                                      variables[1].range_max,
+                                                                                      n_bins,
+                                                                                      variables[2].range_min,
+                                                                                      variables[2].range_max)
+
+   # Otherwise fail
+   else:
+      print "Invalid number of variables: ", len(variables)
+      print "Exiting.."
+      sys.exit()
+
+   tree.Draw(draw_string, cuts)
+   h = getattr(ROOT, tmp_name).Clone()
+   h.SetDirectory(0)
+
+   return h
+# End of MakeHistogram
 
 
 ########################################
@@ -136,12 +238,6 @@ def MakePlots(mis, files, input_treename = 'tree'):
    n_bins_two_var_one_sample = 50  # this is per axis, total bins -> n^2
    n_bins_two_var_two_sample = 50  # this is per axis, total bins -> n^2
    
-
-   # Count the draw commands. This way wec can
-   # assign unique names to the histograms:
-   # htmpX
-   i_draw = 0
-
    # Loop over the list of plots
    li_h2d = []
 
@@ -170,25 +266,18 @@ def MakePlots(mis, files, input_treename = 'tree'):
       # Loop over pairs of variables:
       for ivar1, var1 in enumerate(mi.li_vars):
          
-         #cut_fraction[var1.name] = {}
-         #corr_factor[var1.name] = {}
+         mi_result[var1.name] = {}
 
          for ivar2, var2 in enumerate(mi.li_vars):
-            
-            if ivar2 < ivar1:
-               continue
 
-            if ivar2 > ivar1:
-               continue
-
-               
+            # Diagonal
             if (ivar1 == ivar2):
 
                # For the diagonal we want to calculate:
                # I(T;A) = H(sig,bkg)[A] - f * H(sig)[A] - (1-f) * H(bkg)[A]
 
                # Total cut is
-               # fiducial + range(var1) + extra cut(var1) + range(var2) + extra_cut(var2)
+               # fiducial + range(var1) + extra cut(var1)
                cut = "("
                cut += "({0})".format(mi.fiducial_cut)
                cut += "&&({0}>={1})".format(var1.name, var1.range_min)
@@ -199,107 +288,94 @@ def MakePlots(mis, files, input_treename = 'tree'):
                cut_and_weight_sig = "{0}*{1}".format(cut, extra_weight_sig)
                cut_and_weight_bkg = "{0}*{1}".format(cut, extra_weight_bkg)
 
-               
-               # Build the temporary names for our histograms
-               tmp_name_sig = "htmp{0}".format(i_draw)
-               i_draw += 1
-               tmp_name_bkg = "htmp{0}".format(i_draw)
-               i_draw += 1
-               tmp_name_sigbkg_sig = "htmp{0}".format(i_draw)
-               i_draw += 1
-               tmp_name_sigbkg_bkg = "htmp{0}".format(i_draw)
-               i_draw += 1
+               h_sig = MakeHistogram(input_tree_sig, [var1], n_bins_one_var_one_sample, cut_and_weight_sig)
+               h_bkg = MakeHistogram(input_tree_bkg, [var1], n_bins_one_var_one_sample, cut_and_weight_bkg)
+               h_sigbkg_sig = MakeHistogram(input_tree_sig, [var1], n_bins_one_var_two_sample, cut_and_weight_sig)
+               h_sigbkg_bkg = MakeHistogram(input_tree_bkg, [var1], n_bins_one_var_two_sample, cut_and_weight_bkg)
 
-               
-               # build the drawing-strings
-               # Signal Alone
-               draw_string_sig = "{0}>>{1}({2},{3},{4})".format(var1.name, 
-                                                                tmp_name_sig,
-                                                                n_bins_one_var_one_sample,
-                                                                var1.range_min,
-                                                                var1.range_max)
+            # Below Diagonal
+            elif ivar2 < ivar1:
 
+               # For the off-diagonal we want to calculate:
+               # I(T;A,B) = H(sig,bkg)[A,B] - f * H(sig)[A,B] - (1-f) * H(bkg)[A,B]
 
-               # Background Alone
-               draw_string_bkg = "{0}>>{1}({2},{3},{4})".format(var1.name, 
-                                                                tmp_name_bkg,
-                                                                n_bins_one_var_one_sample,
-                                                                var1.range_min,
-                                                                var1.range_max)
+               # Total cut is
+               # fiducial + range(var1) + extra cut(var1) + range(var2) + extra_cut(var2)
+               cut = "("
+               cut += "({0})".format(mi.fiducial_cut)
+               cut += "&&({0}>={1})".format(var1.name, var1.range_min)
+               cut += "&&({0}<={1})".format(var1.name, var1.range_max)
+               cut += "&&({0})".format(var1.extra_cut)
+               cut += "&&({0}>={1})".format(var2.name, var2.range_min)
+               cut += "&&({0}<={1})".format(var2.name, var2.range_max)
+               cut += "&&({0})".format(var2.extra_cut)
+               cut += ")"
 
-               # Signal for signal+background
-               # (need separate as the binning will be different from sig/bkg alone)
-               draw_string_sigbkg_sig = "{0}>>{1}({2},{3},{4})".format(var1.name, 
-                                                                       tmp_name_sigbkg_sig,
-                                                                       n_bins_one_var_two_sample,
-                                                                       var1.range_min,
-                                                                       var1.range_max)
+               cut_and_weight_sig = "{0}*{1}".format(cut, extra_weight_sig)
+               cut_and_weight_bkg = "{0}*{1}".format(cut, extra_weight_bkg)
 
+               h_sig = MakeHistogram(input_tree_sig, [var1, var2], n_bins_two_var_one_sample, cut_and_weight_sig)
+               h_bkg = MakeHistogram(input_tree_bkg, [var1, var2], n_bins_two_var_one_sample, cut_and_weight_bkg)
+               h_sigbkg_sig = MakeHistogram(input_tree_sig, [var1, var2], n_bins_two_var_two_sample, cut_and_weight_sig)
+               h_sigbkg_bkg = MakeHistogram(input_tree_bkg, [var1, var2], n_bins_two_var_two_sample, cut_and_weight_bkg)
 
-               # Background for signal+background
-               # (need separate as the binning will be different from sig/bkg alone)
-               draw_string_sigbkg_bkg = "{0}>>{1}({2},{3},{4})".format(var1.name, 
-                                                                       tmp_name_sigbkg_bkg,
-                                                                       n_bins_one_var_two_sample,
-                                                                       var1.range_min,
-                                                                       var1.range_max)
+            # Above Diagonal
+            else:
+               continue
+            # End of Diagonal vs Off-Diagonal difference
+            
+            # Combine signal/background
+            h_sigbkg = h_sigbkg_sig
+            h_sigbkg.Add(h_sigbkg_bkg)
 
-               # and actually draw      
-               input_tree_sig.Draw(draw_string_sig, cut_and_weight_sig)
-               input_tree_bkg.Draw(draw_string_bkg, cut_and_weight_bkg)
-               input_tree_sig.Draw(draw_string_sigbkg_sig, cut_and_weight_sig)
-               input_tree_bkg.Draw(draw_string_sigbkg_bkg, cut_and_weight_bkg)
-               
-               # Retrieve the histograms
-               h_sig = getattr(ROOT, tmp_name_sig).Clone()
-               h_bkg = getattr(ROOT, tmp_name_bkg).Clone()
-               h_sigbkg_sig = getattr(ROOT, tmp_name_sigbkg_sig).Clone()
-               h_sigbkg_bkg = getattr(ROOT, tmp_name_sigbkg_bkg).Clone()                              
-               h_sig.SetDirectory(0)
-               h_bkg.SetDirectory(0)
-               h_sigbkg_sig.SetDirectory(0)
-               h_sigbkg_bkg.SetDirectory(0)
+            # Calculate entropies
+            entropy_sig = CalculateEntropy(h_sig)
+            entropy_bkg = CalculateEntropy(h_bkg)
+            entropy_sigbkg = CalculateEntropy(h_sigbkg)
 
-               # Combine signal/background
-               h_sigbkg = h_sigbkg_sig
-               h_sigbkg.Add(h_sigbkg_bkg)
+            # Calculate Mutual Information
+            I = entropy_sigbkg  - f * entropy_sig  - (1-f) * entropy_bkg
+            
+            print "H_sig = {0} \t H_bkg = {1} \t H_sigbkg = {2} \t I = {3}".format(entropy_sig,
+                                                                                   entropy_bkg,
+                                                                                   entropy_sigbkg,
+                                                                                   I)
 
-               # Calculate entropies
-               entropy_sig = CalculateEntropy(h_sig)
-               entropy_bkg = CalculateEntropy(h_bkg)
-               entropy_sigbkg = CalculateEntropy(h_sigbkg)
-               
-               # Calculate Mutual Information
-               I = entropy_sigbkg  - f * entropy_sig  - (1-f) * entropy_bkg
-               
-               print "H_sig = {0} \t H_bkg = {1} \t H_sigbkg = {2} \t I = {3}".format(entropy_sig,
-                                                                                      entropy_bkg,
-                                                                                      entropy_sigbkg,
-                                                                                      I)
-                              
-               
-               h_sig.Draw()
-               h_bkg.Draw("SAME")
-
-               clean_name = var1.name.replace("/","_")
-               clean_name = clean_name.replace("(","")
-               clean_name = clean_name.replace(")","")
-               
-               c.Print(clean_name + ".pdf")
-
-               h_sigbkg.Draw()
-               c.Print(clean_name + "_sum.pdf")
-
+            mi_result[var1.name][var2.name] = I
          # End var2 loop
       # End var1 loop
 
-
       ROOT.gStyle.SetPalette(1)   
+      
+      h_mi = ROOT.TH2F("","", 
+                       len(mi.li_vars), -0.5, -0.5 + len(mi.li_vars),
+                       len(mi.li_vars), -0.5, -0.5 + len(mi.li_vars))      
 
-         
+      for ivar1, var1 in enumerate(mi.li_vars):         
+
+         h_mi.GetXaxis().SetBinLabel(ivar1+1, var1.pretty_name)
+         h_mi.GetYaxis().SetBinLabel(ivar1+1, var1.pretty_name)
+
+         for ivar2, var2 in enumerate(mi.li_vars):
+
+            if ivar2 > ivar1:
+               continue
+
+            h_mi.SetBinContent(ivar1+1, ivar2+1, mi_result[var1.name][var2.name])
+
+
+      h_mi.LabelsOption("v","X")
+      h_mi.GetXaxis().SetLabelSize(0.035)
+      h_mi.GetYaxis().SetLabelSize(0.035)
+      h_mi.GetZaxis().SetLabelSize(0.03)
+      
+      draw_opts = "COLZ TEXT"
+
+      h_mi.Draw(draw_opts)
+      OutputDirectoryHelper.ManyPrint(c, output_dir, "{0}_mi".format(mi.name))
 
    # End mi loop
-# 
+# End MakePlots
             
             
 
