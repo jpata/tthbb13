e563cc416fe7193e8d0152d4937a532616ceeadb
Gregor Kasieczka <g.kasieczka@gmail.com>
2015-01-14 11:12:38 +0100

    Also look at btag

--- Plotting/python/gregor/TestMutualInformation.py
+++ Plotting/python/gregor/TestMutualInformation.py
@@ -35,8 +35,8 @@ for k,v in files.iteritems():
 
 
 mis =[ #mi("taggers", "zprime_m2000_1p", "qcd_800_1000", tagger_vars, fiducial_cut_and_weight),
-       mi("masses", "zprime_m2000_1p", "qcd_800_1000", mass_vars_08+mass_vars_15, fiducial_cut_and_weight),
-       mi("taus", "zprime_m2000_1p", "qcd_800_1000",   tau_vars_08+tau_vars_15+[var.di['ca08softdrop_mass']], fiducial_cut_and_weight),
+       #mi("masses", "zprime_m2000_1p", "qcd_800_1000", mass_vars_08+mass_vars_15, fiducial_cut_and_weight),
+       #mi("taus", "zprime_m2000_1p", "qcd_800_1000",   tau_vars_08+tau_vars_15+[var.di['ca08softdrop_mass']], fiducial_cut_and_weight),
        mi("good", "zprime_m2000_1p", "qcd_800_1000", good_vars, fiducial_cut_and_weight),
 ]
 
