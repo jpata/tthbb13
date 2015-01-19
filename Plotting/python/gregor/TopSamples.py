287e0f447ad6c0202d2fa053efcc7125395c84de
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2015-01-19 13:53:26 +0100

    Added TopSamples, made AddWeight more flexible

---
+++ Plotting/python/gregor/TopSamples.py
@@ -0,0 +1,21 @@
+
+
+files = {}
+
+#files["zprime_m750"]  = "ntop_v15c_zprime_m750_1p_13tev-tagging"     
+files["zprime_m2000"] = "ntop_v15_zprime_m2000_1p_13tev-tagging"     
+
+#files["qcd_170_300"] = "ntop_v15c_qcd_170_300_pythia8_13tev-tagging"
+#files["qcd_300_470"] = "ntop_v15c_qcd_300_470_pythia8_13tev-tagging"
+files["qcd_800_1000"] = "ntop_v15b_qcd_800_1000_pythia8_13tev-tagging"
+
+ranges = {"zprime_m750"  : [201, 299],
+          "zprime_m2000" : [801, 999],
+
+          "qcd_170_300"  : [201, 299],
+          "qcd_300_470"  : [290, 490],
+          "qcd_800_1000" : [801, 999],
+       }
+           
+
+
--- TTHNtupleAnalyzer/python/AddWeights.py
+++ TTHNtupleAnalyzer/python/AddWeights.py
@@ -15,6 +15,7 @@ import pickle
 import ROOT
 
 import TTH.TTHNtupleAnalyzer.AccessHelpers as AH
+from TTH.Plotting.gregor.TopSamples import files 
 
 
 ########################################
@@ -23,99 +24,101 @@ import TTH.TTHNtupleAnalyzer.AccessHelpers as AH
 
 basepath = '/scratch/gregor/'
 
-#input_name = "ntop_v14_zprime_m2000_1p_13tev-tagging"     
-input_name = "ntop_v14_qcd_800_1000_pythia8_13tev-tagging"     
-input_tree_name = "tree"
+for k,v in files.iteritems():
+    print "doing", k
 
-input_pickle_file_name = "/shome/gregor/TTH-73X/CMSSW/src/TTH/Plotting/python/gregor/flat_pt_weights.pickle"
+    input_name = v
+    input_tree_name = "tree"
 
-
-########################################
-# Setup I/O
-########################################
-
-input_root_file_name = basepath + input_name + ".root"
-output_root_file_name = basepath + input_name + "-weighted.root"
-
-input_root_file = ROOT.TFile(input_root_file_name)
-input_tree = input_root_file.Get(input_tree_name)
-
-n_entries = input_tree.GetEntries()
-
-output_root_file = ROOT.TFile(output_root_file_name, 'recreate')
-output_tree = input_tree.CloneTree(0)
+    input_pickle_file_name = "/shome/gregor/TTH-73X/CMSSW/src/TTH/Plotting/python/gregor/flat_pt_weights.pickle"
 
 
-########################################
-# Setup Variables
-########################################
+    ########################################
+    # Setup I/O
+    ########################################
 
-# Create dicitionaries to hold the information that will be
-# written as new branches
-variables      = {}
-variable_types = {}
+    input_root_file_name = basepath + input_name + ".root"
+    output_root_file_name = basepath + input_name + "-weighted.root"
 
-# Setup the output branches for the true object
-AH.addScalarBranches(variables,
-                     variable_types,
-                     output_tree,
-                     ["weight","pt"],
-                     datatype = 'float')
+    input_root_file = ROOT.TFile(input_root_file_name)
+    input_tree = input_root_file.Get(input_tree_name)
 
+    n_entries = input_tree.GetEntries()
 
-########################################
-# Get the weight function
-########################################
-
-# The pickle file contains a dictionary that gives us:
-#   - a function f (fit of the pT spectrum)
-#   - correct variable
-# The weight is actually 1/(f)
-# Example:
-# { "ntop_v8_zprime_m2000_1p_13tev-tagging" : [ROOT.TF1(..), "hadtop"], ...}
-
-pickle_file = open(input_pickle_file_name)
-functions_and_parameter = pickle.load(pickle_file)
-
-fun = functions_and_parameter[input_name][0]
-param_name = functions_and_parameter[input_name][1]
-
-print "Read from file: "
-print fun, param_name
+    output_root_file = ROOT.TFile(output_root_file_name, 'recreate')
+    output_tree = input_tree.CloneTree(0)
 
 
-########################################
-# Event loop
-########################################
+    ########################################
+    # Setup Variables
+    ########################################
 
-print "Processing {0} events".format(n_entries)
+    # Create dicitionaries to hold the information that will be
+    # written as new branches
+    variables      = {}
+    variable_types = {}
 
-for i_event in range(n_entries):
+    # Setup the output branches for the true object
+    AH.addScalarBranches(variables,
+                         variable_types,
+                         output_tree,
+                         ["weight","pt"],
+                         datatype = 'float')
 
-    # Progress
-    if not i_event % 1000:
-        print "{0:.1f}%".format( 100.*i_event /n_entries)
 
-    # Reset branches
-    AH.resetBranches(variables, variable_types)
+    ########################################
+    # Get the weight function
+    ########################################
 
-    input_tree.GetEntry( i_event )    
+    # The pickle file contains a dictionary that gives us:
+    #   - a function f (fit of the pT spectrum)
+    #   - correct variable
+    # The weight is actually 1/(f)
+    # Example:
+    # { "ntop_v8_zprime_m2000_1p_13tev-tagging" : [ROOT.TF1(..), "hadtop"], ...}
 
-    # Calculate the weight
-    pt = AH.getter(input_tree, param_name)
-    value = fun(pt)
-    if value > 0:
-        weight = 1/(value)
-    else:
-        weight = 0
-        
-    variables["weight"][0] = weight
-    variables["pt"][0]     = pt
+    pickle_file = open(input_pickle_file_name)
+    functions_and_parameter = pickle.load(pickle_file)
 
-    output_tree.Fill()
-# End of event loop
-    
+    fun = functions_and_parameter[input_name][0]
+    param_name = functions_and_parameter[input_name][1]
 
-output_tree.AutoSave()
-output_root_file.Close()
-input_root_file.Close()
+    print "Read from file: "
+    print fun, param_name
+
+
+    ########################################
+    # Event loop
+    ########################################
+
+    print "Processing {0} events".format(n_entries)
+
+    for i_event in range(n_entries):
+
+        # Progress
+        if not i_event % 1000:
+            print "{0:.1f}%".format( 100.*i_event /n_entries)
+
+        # Reset branches
+        AH.resetBranches(variables, variable_types)
+
+        input_tree.GetEntry( i_event )    
+
+        # Calculate the weight
+        pt = AH.getter(input_tree, param_name)
+        value = fun(pt)
+        if value > 0:
+            weight = 1/(value)
+        else:
+            weight = 0
+
+        variables["weight"][0] = weight
+        variables["pt"][0]     = pt
+
+        output_tree.Fill()
+    # End of event loop
+
+
+    output_tree.AutoSave()
+    output_root_file.Close()
+    input_root_file.Close()
