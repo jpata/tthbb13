df9416089db6c072010dac7d5f8e604ad39966be
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2015-04-28 11:41:04 +0200

    Removed MEAnalysis directory as this branch is now purely for t/H tagging

--- MEAnalysis/python/branches.py
+++
@@ -1,7 +0,0 @@
-#branch file for METree.hh
-
-#uses branch classes from headergen
-from TTH.TTHNtupleAnalyzer.headergen import *
-
-#uses toptagger branches
-from TTH.TTHNtupleAnalyzer.toptagger_branches import *
--- MEAnalysis/test/runtests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-echo "TTH/MEAnalysis runtests.sh " $LOCAL_TEST_DIR
-cd $CMSSW_BASE/src/TTH/MEAnalysis
-$CMSSW_BASE/bin/$SCRAM_ARCH/MEAnalysis test/me_test_cfg.py
-exit 0
