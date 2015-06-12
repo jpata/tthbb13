7ef7f6e1e484e174a901cc3ff6b1030694e9ca86
Joosep Pata <joosep.pata@cern.ch>
2015-06-12 21:58:57 +0300

    743

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-export SCRAM_ARCH=slc6_amd64_gcc481
+export SCRAM_ARCH=slc6_amd64_gcc491
 
-scram project -n CMSSW CMSSW CMSSW_7_2_2_patch2
+scram project -n CMSSW CMSSW CMSSW_7_4_3
 cd CMSSW/src/
 cmsenv
 git cms-addpkg PhysicsTools/PatAlgos
@@ -13,11 +13,12 @@ cd ..
 #after scram b clean, these need to be copied again
 cp TTH/MEAnalysis/libs/*.so ../lib/$SCRAM_ARCH/
 
+git checkout -b tth743 
 #Get the vhbb-heppy code
-git cms-merge-topic vhbb:vhbbHeppy722patch2
+git cms-merge-topic vhbb:vhbbHeppy743
 
-#Get the fixed TChain code for vhbb
-git cms-merge-topic jpata:heppy-chain-lfn
+##Get the fixed TChain code for vhbb
+#git cms-merge-topic jpata:heppy-chain-lfn
 
 scram setup lhapdf
 
