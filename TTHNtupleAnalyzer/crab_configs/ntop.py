cdbc62a65b3aba2f53fd4b0f57166caccbfd7de7
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2015-07-09 12:24:03 +0200

    ntop60: Run ShowerDecon with mTop 160, mW 73

--- TTHNtupleAnalyzer/crab_configs/ntop.py
+++ TTHNtupleAnalyzer/crab_configs/ntop.py
@@ -15,26 +15,26 @@ import TTH.TTHNtupleAnalyzer.CrabHelpers as CH
 
 # Ntuple name/version and samples to include
 name = "ntop"
-version = "v58a"
+version = "v60"
 li_samples = [
 #    "qcd_300_470_13tev_spring15dr74_asympt50ns",    
 #    "qcd_800_1000_13tev_spring15dr74_asympt50ns",   
 #    "zprime_m1000_1p_13tev_spring15dr74_asympt50ns",
 #    "zprime_m2000_1p_13tev_spring15dr74_asympt50ns",
     
-    "qcd_170_300_13tev_spring15dr74_asympt25ns",    
-    "qcd_300_470_13tev_spring15dr74_asympt25ns",    
+#    "qcd_170_300_13tev_spring15dr74_asympt25ns",    
+#    "qcd_300_470_13tev_spring15dr74_asympt25ns",    
 
 #    "qcd_470_600_13tev_spring15dr74_asympt25ns",    
 #    "qcd_600_800_13tev_spring15dr74_asympt25ns",    
-#    "qcd_800_1000_13tev_spring15dr74_asympt25ns",   
+    "qcd_800_1000_13tev_spring15dr74_asympt25ns",   
 #    "qcd_1000_1400_13tev_spring15dr74_asympt25ns",   
                                                
-    "zprime_m750_1p_13tev_spring15dr74_asympt25ns", 
-    "zprime_m1000_1p_13tev_spring15dr74_asympt25ns",
+#    "zprime_m750_1p_13tev_spring15dr74_asympt25ns", 
+#    "zprime_m1000_1p_13tev_spring15dr74_asympt25ns",
 
 #    "zprime_m1250_1p_13tev_spring15dr74_asympt25ns",
-#    "zprime_m2000_1p_13tev_spring15dr74_asympt25ns",
+    "zprime_m2000_1p_13tev_spring15dr74_asympt25ns",
 #    "zprime_m3000_1p_13tev_spring15dr74_asympt25ns",
 
 
