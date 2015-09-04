d0e6693a93b3fd17484469d4da553f44e71cc7b3
Joosep <joosep.pata@cern.ch>
2015-09-04 16:00:34 +0200

    longer runtime for smaller jobs

--- MEAnalysis/crab/heppy_crab_config.py
+++ MEAnalysis/crab/heppy_crab_config.py
@@ -13,7 +13,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'heppy_crab_fake_pset.py'
 config.JobType.scriptExe = 'heppy_crab_script.sh'
 #job maximum runtime in minutes
-config.JobType.maxJobRuntimeMin = 12 * 60
+config.JobType.maxJobRuntimeMin = 47 * 60
 
 import os
 #we need to specially ship the python and data directories
