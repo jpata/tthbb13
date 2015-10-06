80591f3665a6c89d7923147a3268c22c952cd416
Joosep Pata <joosep.pata@cern.ch>
2015-10-06 20:45:55 +0300

    new root version only for melooper

--- setenv_kbfi.sh
+++ setenv_kbfi.sh
@@ -2,4 +2,3 @@ eval `scramv1 runtime -sh`
 export PYTHONPATH=~joosep/anaconda/lib/python2.7/site-packages:$PYTHONPATH
 export LD_LIBRARY_PATH=~joosep/anaconda/lib/:$LD_LIBRARY_PATH
 export PATH=~joosep/anaconda/bin:~joosep/.julia/v0.4/ROOT/:$PATH
-source /home/joosep/root-6.05.02/bin/thisroot.sh
