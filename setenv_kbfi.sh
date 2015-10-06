8cf4bd56e8d254a34e5a288f22acc77fa623642a
Joosep Pata <joosep.pata@cern.ch>
2015-10-06 20:39:00 +0300

    makeJobfiles now has categories

--- setenv_kbfi.sh
+++ setenv_kbfi.sh
@@ -1,4 +1,5 @@
 eval `scramv1 runtime -sh`
 export PYTHONPATH=~joosep/anaconda/lib/python2.7/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=~joosep/anaconda/lib/python2.7/site-packages:$LD_LIBRARY_PATH
-export PATH=~joosep/anaconda/bin:$PATH
+export LD_LIBRARY_PATH=~joosep/anaconda/lib/:$LD_LIBRARY_PATH
+export PATH=~joosep/anaconda/bin:~joosep/.julia/v0.4/ROOT/:$PATH
+source /home/joosep/root-6.05.02/bin/thisroot.sh
