3cd70817100aefe28821406d534234d2e3a73961
Joosep Pata <joosep.pata@cern.ch>
2015-10-08 20:20:51 +0300

    use prev13 samples

--- Plotting/python/Datacards/MiniSamples.py
+++ Plotting/python/Datacards/MiniSamples.py
@@ -14,14 +14,16 @@ if "t3ui12" in hostname:
 elif "pata-slc6" in hostname:
     path = "/home/joosep/joosep-mac/Documents/tth/data/ntp/v12/Sep9_jec_jer/"
 #Tallinn
+#elif "hep.kbfi.ee" in hostname:
+#    path = "/home/joosep/tth/gc/"
 elif "hep.kbfi.ee" in hostname:
-    path = "/home/joosep/tth/gc/"
+    path = "/hdfs/cms/store/user/jpata/tth/Sep28_spring15_prev13_f6c61f/"
 else:
     raise Exception("Sample path not defined for hostname={0}!".format(hostname))
 
-samples_dict = {    
+samples_dict = {
     "ttH_hbb"        : path + "ttHTobb_M125_13TeV_powheg_pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.root",
-    #"ttH_nohbb"      : path + "ttHToNonbb_M125_13TeV_powheg_pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2.root",        
+    "ttH_nohbb"      : path + "ttHToNonbb_M125_13TeV_powheg_pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2.root",        
     "ttbarPlus2B"    : path + "TT_TuneCUETP8M1_13TeV-powheg-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_tt2b.root",
     "ttbarPlusB"     : path + "TT_TuneCUETP8M1_13TeV-powheg-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ttb.root",        
     "ttbarPlusBBbar" : path + "TT_TuneCUETP8M1_13TeV-powheg-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ttbb.root",
@@ -32,6 +34,3 @@ samples_dict = {
     #"ttz_zllnunu"    : path + "TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.root",
     #"ttz_zqq"        : path + "TTZToQQ_TuneCUETP8M1_13TeV-amcatnlo-pythia8__RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1.root",
 }
-
-
-
