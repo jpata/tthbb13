6d912d14393b2d7da4927c66accd867210d7314b
Joosep Pata <joosep.pata@cern.ch>
2015-11-02 14:55:14 +0200

    added additional loose e trigger

--- MEAnalysis/python/MEAnalysis_heppy.py
+++ MEAnalysis/python/MEAnalysis_heppy.py
@@ -301,7 +301,7 @@ if __name__ == "__main__":
 
         #Configure the number of events to run
         from PhysicsTools.HeppyCore.framework.looper import Looper
-        nEvents = 1000
+        nEvents = 5000
 
 
         kwargs = {}
