1ec6b38845a6c60871a43499c2322d1d8d6f4ea2
d-salerno <daniel.salerno@cern.ch>
2016-01-13 15:27:31 +0100

    add european T2s to crab whitelists

--- MEAnalysis/crab/cfg_V14.py
+++ MEAnalysis/crab/cfg_V14.py
@@ -1,22 +1,22 @@
 from WMCore.Configuration import Configuration
 config = Configuration()
 
-#****************************DS CHANGE THIS && samples_v14.py (set to False) && inputfilename/script && COMPILE ****
-# processing_name = "TTH_v3" #v1 done with QCD300 in samples_V14, v2 ok
-# processing_name = "TTJets_v2" #v1 done with TTJets??
-# processing_name = "QCD300_v2" #v1 done with QCD300 in samples
+#****************************DS CHANGE THIS && samples_v14.py (set to False) && userInputFiles/script && COMPILE ****
+# processing_name = "TTH_v1"
+# processing_name = "TTJets_v1"
+# processing_name = "QCD300_v1"
 # processing_name = "QCD500_v1"
-# processing_name = "QCD700_v3" #v2 done with QCD700 in samples
-processing_name = "QCD1000_v1"
+# processing_name = "QCD700_v1"
+# processing_name = "QCD1000_v" #forgot the v1
 # processing_name = "QCD1500_v1"
-# processing_name = "QCD2000_v5" #v3 done with QCD700 in samples_V14
+processing_name = "QCD2000_v1"
 
 sample = processing_name.split("_")[0]
 
 config.section_("General")
 #this will be used for the crab directory name
 config.General.requestName = processing_name #DS
-config.General.workArea = 'crab_projects_mem/' #DS
+config.General.workArea = 'crab_projects_mem_V14_all/' #DS
 config.General.transferLogs = True
 
 config.section_("JobType")
@@ -24,7 +24,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'heppy_crab_fake_pset.py'
 config.JobType.scriptExe = 'heppy_crab_script.sh'
 #job maximum runtime in minutes
-config.JobType.maxJobRuntimeMin = 46 * 60
+config.JobType.maxJobRuntimeMin = 2750 # 46 * 60
 
 import os
 #we need to specially ship the python and data directories
@@ -44,16 +44,19 @@ config.JobType.inputFiles = [
 
 config.section_("Data")
 #this name will be used for creating the output directory
-config.Data.primaryDataset = "/" + sample + "/V14/MEM" #DS
+config.Data.outputPrimaryDataset = sample #"/" + sample + "/V14/MEM" #DS
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
-config.Data.outLFNDirBase = '/store/user/dsalerno/tthbb745/VHBBHeppyV14/' #DS
+config.Data.outLFNDirBase = '/store/user/dsalerno/tthbb745/VHBBHeppyV14_all/' #DS
 config.Data.publication = False
-config.Data.userInputFiles = open( sample+".txt").readlines() #DS
+#config.Data.userInputFiles = open( sample+".txt").readlines() #DS
+config.Data.userInputFiles = open( sample+".jobs").readlines() #DS change in heppy_crab_script.py
 
 config.section_("Site")
 #config.Site.storageSite = "T2_CH_CSCS"
 config.Site.storageSite = "T3_CH_PSI" #DS
+config.Site.blacklist = ["T3_US_FIT*"]
+config.Site.whitelist = ["T2_IT*","T2_CH*","T2_FR*", "T2_DE*"]
 
 #config.Data.ignoreLocality = True
