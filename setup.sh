7412f63d347060c250c0b38cb03df166d6d200da
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2016-04-20 17:48:29 +0200

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-export SCRAM_ARCH=slc6_amd64_gcc491
+export SCRAM_ARCH=slc6_amd64_gcc530
 
-scram project -n CMSSW CMSSW CMSSW_7_4_1_patch1
+scram project -n CMSSW CMSSW CMSSW_8_0_4
 cd CMSSW/src/
 cmsenv
 git cms-addpkg PhysicsTools/PatAlgos
@@ -13,11 +13,11 @@ git cms-addpkg RecoJets/JetProducers
 # get the TTH code
 git clone https://github.com/jpata/tthbb13.git TTH
 cd TTH
-git checkout dev-74X
+git checkout tagging-80X
 cd ..
 
 # Add HEPTopTagger
-git cms-merge-topic gkasieczka:htt-v2-74X
+git cms-merge-topic gkasieczka:htt-v2-76X
 
 cp $CMSSW_BASE/external/$SCRAM_ARCH/lib/* $CMSSW_BASE/lib/$SCRAM_ARCH/
 
