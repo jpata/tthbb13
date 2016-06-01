7282107d55a9c1bb0cc184278797bad30ebdfad0
Joosep Pata <joosep.pata@cern.ch>
2016-06-01 14:16:58 +0300

    move to 80X

--- setup.sh
+++ setup.sh
@@ -1,22 +1,24 @@
-export SCRAM_ARCH=slc6_amd64_gcc493
+export SCRAM_ARCH=slc6_amd64_gcc530
 
-scram project -n CMSSW CMSSW CMSSW_7_6_3_patch2
+scram project -n CMSSW CMSSW CMSSW_8_0_5
 cd CMSSW/src/
 eval `scramv1 runtime -sh`
 
 git cms-init
-git cms-merge-topic jpata:V21_v1
+git cms-merge-topic vhbb:vhbbHeppy80X
 
 #get the TTH code
 git clone https://github.com/jpata/tthbb13.git TTH
 cd $CMSSW_BASE/src/TTH
-git clone https://github.com/bianchini/Code.git MEIntegratorStandalone
-git clone https://github.com/jpata/CommonClassifier.git CommonClassifier --branch mem_npoints
+git checkout master
+git clone https://github.com/bianchini/Code.git MEIntegratorStandalone --branch v0.2
+git clone https://github.com/cms-ttH/CommonClassifier.git CommonClassifier --branch master
 git clone https://github.com/grid-control/grid-control MEAnalysis/gc/grid-control
 
 cd $CMSSW_BASE/src
 
-git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit --branch 74x-root6
+#FIXME: combine is not yet 80X?
+#git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit --branch 74x-root6
 #after scram b clean, these need to be copied again
 cp -R TTH/MEIntegratorStandalone/libs/* ../lib/$SCRAM_ARCH/
 scram setup lhapdf
