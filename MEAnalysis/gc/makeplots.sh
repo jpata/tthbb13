89bc27743823c58b79d174e1ca07ab94895eeab6
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2016-06-07 11:12:44 +0200

    Boosted control plots

--- MEAnalysis/gc/makeplots.sh
+++ MEAnalysis/gc/makeplots.sh
@@ -5,7 +5,7 @@ cd $GC_SCRATCH
 
 # Run Plotting
 echo "Running plotting"
-python ${CMSSW_BASE}/src/TTH/Plotting/python/gregor/HiggsMasses.py 
+python ${CMSSW_BASE}/src/TTH/Plotting/python/gregor/BoostControlPlots.py 
 echo "Done plotting"
 
 
