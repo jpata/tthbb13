bbbc9476396d3f471ef58e451838e05cb3c0697a
Joosep Pata <joosep.pata@gmail.com>
2016-07-30 16:48:10 +0200

    improve local calcme check

--- MEAnalysis/python/cfg_local.py
+++ MEAnalysis/python/cfg_local.py
@@ -2,4 +2,9 @@ from TTH.MEAnalysis.MEAnalysis_cfg_heppy import Conf
 import os
 
 Conf.general["sampleFile"] = os.environ["CMSSW_BASE"] + "/src/TTH/MEAnalysis/python/samples_local.py"
-Conf.mem["calcME"] = bool(os.environ.get("TTH_CALCME", 0))
+calcME = int(os.environ.get("TTH_CALCME", 0))
+if calcME == 1:
+    calcME = True
+else:
+    calcME = False
+Conf.mem["calcME"] = calcME
